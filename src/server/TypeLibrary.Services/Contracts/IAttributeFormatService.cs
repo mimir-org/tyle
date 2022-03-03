6a496b3bad2ffc682ef2b0d81667e1816a1be2b5
arisasel <arisa.seljestokken@bouvet.no>
2022-03-03 13:42:33 +0100

    added id at PUT-endpoint for AttributeFormat

--- src/server/TypeLibrary.Services/Contracts/IAttributeFormatService.cs
+++ src/server/TypeLibrary.Services/Contracts/IAttributeFormatService.cs
@@ -8,7 +8,7 @@ namespace TypeLibrary.Services.Contracts
     public interface IAttributeFormatService
     {
         Task<IEnumerable<AttributeFormatLibCm>> GetAttributeFormats();
-        Task<AttributeFormatLibCm> UpdateAttributeFormat(AttributeFormatLibAm dataAm);
+        Task<AttributeFormatLibCm> UpdateAttributeFormat(AttributeFormatLibAm dataAm, string id);
         Task<AttributeFormatLibCm> CreateAttributeFormat(AttributeFormatLibAm dataAm);
         Task CreateAttributeFormats(List<AttributeFormatLibAm> dataAm);
     }
--- src/server/TypeLibrary.Services/Services/AttributeFormatService.cs
+++ src/server/TypeLibrary.Services/Services/AttributeFormatService.cs
@@ -34,9 +34,10 @@ namespace TypeLibrary.Services.Services
             return Task.FromResult(dataAm.AsEnumerable());
         }
 
-        public async Task<AttributeFormatLibCm> UpdateAttributeFormat(AttributeFormatLibAm dataAm)
+        public async Task<AttributeFormatLibCm> UpdateAttributeFormat(AttributeFormatLibAm dataAm, string id)
         {
             var data = _mapper.Map<AttributeFormatLibDm>(dataAm);
+            data.Id = int.Parse(id);
             data.Updated = DateTime.Now.ToUniversalTime();
             data.UpdatedBy = _contextAccessor?.GetName() ?? "Unknown";
             _formatRepository.Update(data);
