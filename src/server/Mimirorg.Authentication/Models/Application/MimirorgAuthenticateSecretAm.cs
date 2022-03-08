62e04da04179cc78cb6f2e9e1d1483896e43acb7
arisasel <arisa.seljestokken@bouvet.no>
2022-03-08 14:40:59 +0100

    changed endpoint mimirorgauthenticate with secret to POST from GET
    the endpoint now requires a secret in body instead of the route/url

---
+++ src/server/Mimirorg.Authentication/Models/Application/MimirorgAuthenticateSecretAm.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mimirorg.Authentication.Models.Application
+{
+    public class MimirorgAuthenticateSecretAm
+    {
+        [Display(Name = "Secret")]
+        [Required(ErrorMessage = "{0} is required")]
+        public string Secret { get; set; }
+    }
+}
