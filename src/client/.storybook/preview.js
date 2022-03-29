07cc09fbd699e315f3bdfccb0839ed9ab45ce85d
Olav Gjerde <fruithut@tutanota.com>
2022-03-29 17:21:59 +0200

    [CLIENT] Adds Storybook to solution

---
+++ src/client/.storybook/preview.js
@@ -0,0 +1,20 @@
+import { GlobalStyle } from "../src/compLibrary/core";
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+
+export const decorators = [
+  (Story) => (
+    <>
+      <GlobalStyle />
+      <Story />
+    </>
+  ),
+];
