dc4d280b4b5c3ce2fc9203600206557d3342deb7
Olav Gjerde <fruithut@tutanota.com>
2022-04-05 17:55:21 +0200

    [CLIENT] Expands generic flex component

--- src/client/src/compLibrary/layout/Flex.ts
+++ src/client/src/compLibrary/layout/Flex.ts
@@ -5,6 +5,12 @@ interface FlexProps {
   flexDirection?: "column" | "row";
   alignItems?: string;
   justifyContent?: string;
+  padding?: string;
+  border?: string;
+  borderRadius?: string;
+  boxShadow?: string;
+  width?: string;
+  height?: string;
 }
 
 export const Flex = styled.div<FlexProps>`
@@ -13,6 +19,15 @@ export const Flex = styled.div<FlexProps>`
   flex-direction: ${(props) => props.flexDirection};
   align-items: ${(props) => props.alignItems};
   justify-content: ${(props) => props.justifyContent};
+
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+
+  padding: ${(props) => props.padding};
+  border: ${(props) => props.border};
+  border-radius: ${(props) => props.borderRadius};
+
+  box-shadow: ${(props) => props.boxShadow};
 `;
 
 Flex.defaultProps = {
