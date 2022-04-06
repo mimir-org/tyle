fb85ef90960f8488bb21d0ddb4a378091f2ff3b6
Olav Gjerde <fruithut@tutanota.com>
2022-04-06 11:27:22 +0200

    [CLIENT] Adds element rounding overview story

--- src/client/src/compLibrary/core/variables/variablesBorder.ts
+++ src/client/src/compLibrary/core/variables/variablesBorder.ts
@@ -2,7 +2,6 @@ import { css } from "styled-components/macro";
 
 export const variablesBorder = css`
   :root {
-    --border-radius-xs: 2px;
     --border-radius-small: 4px;
     --border-radius-medium: 8px;
     --border-radius-large: 16px;
@@ -11,7 +10,6 @@ export const variablesBorder = css`
 
 export const BORDER = {
   RADIUS: {
-    XS: "var(--border-radius-xs)",
     SMALL: "var(--border-radius-small)",
     MEDIUM: "var(--border-radius-medium)",
     LARGE: "var(--border-radius-large)",
