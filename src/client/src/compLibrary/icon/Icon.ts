ac56e66d0a5e02bcbb591a518fd0f7b89c541179
Olav Gjerde <fruithut@tutanota.com>
2022-04-06 13:44:13 +0200

    [CLIENT] Adjusts Icon component to scale with font

--- src/client/src/compLibrary/icon/Icon.ts
+++ src/client/src/compLibrary/icon/Icon.ts
@@ -6,14 +6,12 @@ interface Props {
 
 /**
  * A simple wrapper over the img-tag
- * Has a default width and height of 15px
+ * Has a default width and height of 1em
  * @param size sets height and width of icon
  */
 export const Icon = styled.img<Props>`
-  width: ${(props) => props.size + "px"};
-  height: ${(props) => props.size + "px"};
+  display: inline-block;
+  width: ${(props) => (props.size ? `${props.size}px` : "1em")};
+  height: ${(props) => (props.size ? `${props.size}px` : "1em")};
+  line-height: 1;
 `;
-
-Icon.defaultProps = {
-  size: 16,
-};
