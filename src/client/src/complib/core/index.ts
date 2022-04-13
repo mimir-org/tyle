902a6e454ab224105ac8ceeca5fd27d06acde1bb
Olav Gjerde <fruithut@tutanota.com>
2022-04-13 15:34:36 +0200

    [CLIENT] Adds global animation constants
    - These are meant to hold common motions that the application may need, e.g: fading, sliding, expanding, shrinking, zooming

--- src/client/src/complib/core/index.ts
+++ src/client/src/complib/core/index.ts
@@ -1,3 +1,4 @@
 export { GlobalStyle } from "./globalStyle";
 export { QUERIES } from "./constants/queries";
 export { THEME } from "./constants/theme";
+export { ANIMATION } from "./constants/animation";
