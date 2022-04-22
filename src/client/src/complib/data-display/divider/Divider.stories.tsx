fe9df6e0a95dab2d36228b3c1de68bd0efa5ce61
Olav Gjerde <fruithut@tutanota.com>
2022-04-22 11:14:50 +0200

    [CLIENT] Extends Divider.ts with variants for vertical and horizontal orientation

--- src/client/src/complib/data-display/divider/Divider.stories.tsx
+++ src/client/src/complib/data-display/divider/Divider.stories.tsx
@@ -1,8 +1,19 @@
 import { Divider } from "./Divider";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
 
 export default {
   title: "Data display/Divider",
   component: Divider,
+} as ComponentMeta<typeof Divider>;
+
+const Template: ComponentStory<typeof Divider> = (args) => <Divider {...args} />;
+
+export const Horizontal = Template.bind({});
+Horizontal.args = {
+  variant: "horizontal",
 };
 
-export const Default = <Divider />;
+export const Vertical = Template.bind({});
+Vertical.args = {
+  variant: "vertical",
+};
