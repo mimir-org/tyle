9e6e9b6cfe30c86bf1de7cb1498e308839dae0f2
Kai Andre Hovden <kai.hovden@bouvet.no>
2022-05-02 11:00:35 +0200

    #69759 Fixed bug in BlobService (mapping)

--- src/server/TypeLibrary.Core/Profiles/BlobProfile.cs
+++ src/server/TypeLibrary.Core/Profiles/BlobProfile.cs
@@ -10,11 +10,6 @@ namespace TypeLibrary.Core.Profiles
     {
         public BlobProfile(IApplicationSettingsRepository settings)
         {
-            CreateMap<BlobLibDm, BlobLibAm>()
-                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
-                .ForMember(dest => dest.Data, opt => opt.MapFrom(src => src.Data))
-                .ForMember(dest => dest.Discipline, opt => opt.MapFrom(src => src.Discipline));
-
             CreateMap<BlobLibAm, BlobLibDm>()
                 .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
                 .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
@@ -22,12 +17,17 @@ namespace TypeLibrary.Core.Profiles
                 .ForMember(dest => dest.Data, opt => opt.MapFrom(src => src.Data))
                 .ForMember(dest => dest.Discipline, opt => opt.MapFrom(src => src.Discipline));
 
+            CreateMap<BlobLibDm, BlobLibAm>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Data, opt => opt.MapFrom(src => src.Data))
+                .ForMember(dest => dest.Discipline, opt => opt.MapFrom(src => src.Discipline));
+
             CreateMap<BlobLibDm, BlobLibCm>()
                 .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
                 .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
                 .ForMember(dest => dest.Iri, opt => opt.MapFrom(src => src.Iri))
-                .ForMember(dest => dest.Data, opt => opt.MapFrom(src => src.Data))
-                .ForMember(dest => dest.Discipline, opt => opt.MapFrom(src => src.Discipline));
+                .ForMember(dest => dest.Discipline, opt => opt.MapFrom(src => src.Discipline))
+                .ForMember(dest => dest.Data, opt => opt.MapFrom(src => src.Data));
         }
     }
 }
