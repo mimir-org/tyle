e58465ecf595973800a991b478407338056193df
Olav Gjerde <fruithut@tutanota.com>
2022-05-05 17:30:19 +0200

    [CLIENT] Adds from and scale animations to variablesAnimation.ts

--- src/client/src/complib/core/variables/variablesAnimation.ts
+++ src/client/src/complib/core/variables/variablesAnimation.ts
@@ -1,6 +1,8 @@
 export interface AnimationSystem {
   fade: Record<string, unknown>,
+  scale: Record<string, unknown>,
   selectHover: Record<string, unknown>,
+  from: (direction: "top" | "right" | "bottom" | "left") => Record<string, unknown>
 }
 
 export const animation: AnimationSystem = {
@@ -14,6 +16,42 @@ export const animation: AnimationSystem = {
     exit: {
       opacity: 0,
     },
+  },
+  scale: {
+    initial: {
+      scale: 0.8,
+    },
+    animate: {
+      scale: 1,
+    },
+    exit: {
+      scale: 0.8,
+    },
+  },
+  from(direction: "top" | "right" | "bottom" | "left", distance = 10) {
+    const fromToMap = {
+      top: {
+        y: `${distance}px`
+      },
+      right: {
+        x: `-${distance}px`
+      },
+      bottom: {
+        y: `-${distance}px`
+      },
+      left: {
+        x: `${distance}px`
+      }
+    }
+
+    return {
+      initial: fromToMap[direction],
+      animate: {
+        x: 0,
+        y: 0,
+      },
+      exit: fromToMap[direction]
+    }
   },
   selectHover: {
     whileHover: {
