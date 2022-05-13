bc2af67dd1c1dff70f868296a623ba3e3db22802
Olav Gjerde <fruithut@tutanota.com>
2022-05-13 17:52:43 +0200

    [CLIENT] Adjusts title sizes for RegisterFinalize component

--- src/client/src/content/forms/register/components/RegisterFinalize.tsx
+++ src/client/src/content/forms/register/components/RegisterFinalize.tsx
@@ -2,7 +2,7 @@ import { TextResources } from "../../../../assets/text";
 import { Flexbox } from "../../../../complib/layouts";
 import { RegisterFinalizeLink, RegisterQrImage } from "./RegisterFinalize.styled";
 import { Divider } from "../../../../complib/data-display";
-import { Heading, Text } from "../../../../complib/text";
+import { Text } from "../../../../complib/text";
 import { useTheme } from "styled-components";
 
 interface Props {
@@ -14,13 +14,13 @@ export const RegisterFinalize = ({ qrCodeBase64 }: Props) => {
 
   return (
     <Flexbox flexDirection={"column"} gap={theme.tyle.spacing.xl}>
-      <Flexbox as={"section"} flexDirection={"column"} gap={theme.tyle.spacing.xl}>
-        <Heading>{TextResources.REGISTER_FINALIZE_VERIFICATION}</Heading>
+      <Flexbox as={"section"} flexDirection={"column"} gap={theme.tyle.spacing.medium}>
+        <Text variant={"display-small"}>{TextResources.REGISTER_FINALIZE_VERIFICATION}</Text>
         <Text>{TextResources.REGISTER_FINALIZE_VERIFICATION_DESCRIPTION}</Text>
       </Flexbox>
       <Divider />
-      <Flexbox as={"section"} flexDirection={"column"} gap={theme.tyle.spacing.xl}>
-        <Heading>{TextResources.REGISTER_FINALIZE_MFA}</Heading>
+      <Flexbox as={"section"} flexDirection={"column"} gap={theme.tyle.spacing.medium}>
+        <Text variant={"display-small"}>{TextResources.REGISTER_FINALIZE_MFA}</Text>
         <Text>{TextResources.REGISTER_FINALIZE_MFA_DESCRIPTION}</Text>
         <RegisterQrImage size={300} src={qrCodeBase64} alt="" />
         <RegisterFinalizeLink to="/">{TextResources.REGISTER_FINALIZE_FINISH_LINK}</RegisterFinalizeLink>
