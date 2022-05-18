9a4d122e4ce7fb8295a9f8b980b74e2cfab0a69a
Olav Gjerde <fruithut@tutanota.com>
2022-05-18 16:02:01 +0200

    [CLIENT] Adjusts flex props and mixin

--- src/client/src/complib/mixins/flexMixin.ts
+++ src/client/src/complib/mixins/flexMixin.ts
@@ -2,15 +2,16 @@ import { css } from "styled-components/macro";
 import { Flex } from "../props";
 
 export const flexMixin = css<Flex>`
-  flex-direction: ${(props) => props.flexDirection};
+  flex: ${(props) => props.flex};
+  flex-grow: ${(props) => props.flexGrow};
   flex-wrap: ${(props) => props.flexWrap};
+  flex-shrink: ${(props) => props.flexShrink};
+  flex-direction: ${(props) => props.flexDirection};
+  flex-flow: ${(props) => props.flexFlow};
   justify-content: ${(props) => props.justifyContent};
   align-items: ${(props) => props.alignItems};
   align-content: ${(props) => props.alignContent};
-  order: ${(props) => props.order};
-  flex: ${(props) => props.flex};
-  flex-grow: ${(props) => props.flexGrow};
-  flex-shrink: ${(props) => props.flexShrink};
   align-self: ${(props) => props.alignSelf};
+  order: ${(props) => props.order};
   gap: ${(props) => props.gap};
 `;
