cf3292456428a2ec7934ab79df5f00a895fb4a37
Olav Gjerde <fruithut@tutanota.com>
2022-05-19 14:58:48 +0200

    [CLIENT] Adjusts ellipsisMixin's imports and exports

--- src/client/src/complib/mixins/index.ts
+++ src/client/src/complib/mixins/index.ts
@@ -8,6 +8,7 @@ export { shadowsMixin } from "./props/shadowsMixin";
 export { spacingMixin } from "./props/spacingMixin";
 export { sizingMixin } from "./props/sizingMixin";
 export { typographyMixin } from "./props/typographyMixin";
+export { ellipsisMixin } from "./props/ellipsisMixin";
 export { getTextRole } from "./getTextRole";
 export { layer } from "./layer";
 export { translucify } from "./translucify";
