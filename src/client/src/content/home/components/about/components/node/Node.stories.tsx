5475d19fb2d4892bc78e8779fb447cef32298178
Olav Gjerde <fruithut@tutanota.com>
2022-05-20 16:27:52 +0200

    [CLIENT] Adjusts Node default story for reuse in other component stories

--- src/client/src/content/home/components/about/components/node/Node.stories.tsx
+++ src/client/src/content/home/components/about/components/node/Node.stories.tsx
@@ -4,12 +4,12 @@ import { Node } from "./Node";
 export default {
   title: "Content/Home/About/Node/Node",
   component: Node,
-  args: {
-    color: "#fef445",
-    img: "static/media/src/assets/icons/modules/library.svg",
-  },
 } as ComponentMeta<typeof Node>;
 
 const Template: ComponentStory<typeof Node> = (args) => <Node {...args} />;
 
 export const Default = Template.bind({});
+Default.args = {
+  color: "#fef445",
+  img: "static/media/src/assets/icons/modules/library.svg",
+};
