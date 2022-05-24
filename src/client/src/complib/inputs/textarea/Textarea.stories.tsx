62a14e944e7eda5472aab9e97cf9014609fe2043
Olav Gjerde <fruithut@tutanota.com>
2022-05-24 14:06:11 +0200

    [CLIENT] Adds Textarea component

---
+++ src/client/src/complib/inputs/textarea/Textarea.stories.tsx
@@ -0,0 +1,15 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { Textarea } from "./Textarea";
+
+export default {
+  title: "Inputs/Textarea",
+  component: Textarea,
+} as ComponentMeta<typeof Textarea>;
+
+const Template: ComponentStory<typeof Textarea> = (args) => <Textarea {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  defaultValue: "Some input",
+  placeholder: "placeholder",
+};
