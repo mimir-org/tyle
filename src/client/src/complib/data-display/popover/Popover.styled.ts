a3fec361a013ce732d6e4aaf4d9797e03c336ff4
Olav Gjerde <fruithut@tutanota.com>
2022-06-02 14:13:58 +0200

    [CLIENT] Increases padding in popovers
    - Since popovers usually are content heavy, a larger padding size suits them better

--- src/client/src/complib/data-display/popover/Popover.styled.ts
+++ src/client/src/complib/data-display/popover/Popover.styled.ts
@@ -2,7 +2,7 @@ import styled from "styled-components/macro";
 import { motion } from "framer-motion";
 
 const PopoverContent = styled.div`
-  padding: ${(props) => props.theme.tyle.spacing.xs};
+  padding: ${(props) => props.theme.tyle.spacing.small};
   border-radius: ${(props) => props.theme.tyle.border.radius.medium};
   background-color: ${(props) => props.theme.tyle.color.surface.inverse.base};
   color: ${(props) => props.theme.tyle.color.surface.inverse.on};
