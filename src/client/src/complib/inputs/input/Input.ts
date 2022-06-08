e0c10f3050f429803f8d42fa287a90ff4d737b31
Olav Gjerde <fruithut@tutanota.com>
2022-06-08 16:09:26 +0200

    [CLIENT] Extends Input component with sizing props

--- src/client/src/complib/inputs/input/Input.ts
+++ src/client/src/complib/inputs/input/Input.ts
@@ -1,10 +1,14 @@
+import { InputHTMLAttributes } from "react";
 import styled from "styled-components";
-import { getTextRole } from "../../mixins";
+import { getTextRole, sizingMixin } from "../../mixins";
+import { Sizing } from "../../props";
+
+type InputProps = InputHTMLAttributes<HTMLInputElement> & Sizing;
 
 /**
  * A simple wrapper over the input-tag, with styling that follows library conventions.
  */
-export const Input = styled.input`
+export const Input = styled.input<InputProps>`
   height: 40px;
   min-width: 250px;
 
@@ -13,6 +17,7 @@ export const Input = styled.input`
   border-radius: ${(props) => props.theme.tyle.border.radius.small};
 
   ${getTextRole("body-large")};
+  ${sizingMixin};
 
   ::placeholder {
     color: ${(props) => props.theme.tyle.color.outline.base};
