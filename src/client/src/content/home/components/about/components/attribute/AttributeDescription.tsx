56383b29e698bd1fb1440c5bdb6175684ff5b9ab
Olav Gjerde <fruithut@tutanota.com>
2022-06-21 14:12:44 +0200

    [CLIENT] Adjusts AttributeDescription according to design

--- src/client/src/content/home/components/about/components/attribute/AttributeDescription.tsx
+++ src/client/src/content/home/components/about/components/attribute/AttributeDescription.tsx
@@ -1,5 +1,6 @@
 import { useTheme } from "styled-components";
 import { Button } from "../../../../../../complib/buttons";
+import { Divider } from "../../../../../../complib/data-display";
 import { Box, Flexbox } from "../../../../../../complib/layouts";
 import { Text } from "../../../../../../complib/text";
 import { Actionable } from "../../../../../../types";
@@ -30,40 +31,31 @@ export const AttributeDescription = ({
   const theme = useTheme();
 
   return (
-    <Box
-      as={"section"}
-      position={"relative"}
-      display={"flex"}
-      flexDirection={"column"}
-      width={"150px"}
-      gap={theme.tyle.spacing.xl}
-    >
-      <Flexbox justifyContent={"space-between"} alignItems={"start"} gap={theme.tyle.spacing.base}>
-        <Text variant={"title-medium"}>{name}</Text>
-        {actionable && onAction && (
-          <Button
-            variant={"filled"}
-            icon={actionIcon}
-            iconOnly
-            onClick={onAction}
-            color={theme.tyle.color.sys.surface.on}
-          >
-            {actionText}
-          </Button>
-        )}
+    <Box as={"section"} display={"flex"} flexDirection={"column"} width={"118px"} gap={theme.tyle.spacing.l}>
+      <Flexbox flexDirection={"column"} gap={theme.tyle.spacing.xs}>
+        <Flexbox justifyContent={"space-between"}>
+          <Text variant={"title-small"}>{name}</Text>
+          {actionable && onAction && (
+            <Button
+              variant={"filled"}
+              color={theme.tyle.color.sys.surface.on}
+              onClick={onAction}
+              icon={actionIcon}
+              iconOnly
+            >
+              {actionText}
+            </Button>
+          )}
+        </Flexbox>
+        <Divider />
       </Flexbox>
 
-      <Flexbox flexDirection={"column"} gap={theme.tyle.spacing.s}>
+      <Flexbox flexDirection={"column"} gap={theme.tyle.spacing.base}>
         {traits &&
           Object.keys(traits).map((k, i) => (
-            <Flexbox key={i} gap={theme.tyle.spacing.xl} justifyContent={"space-between"}>
-              <Text variant={"body-medium"} textTransform={"capitalize"}>
-                {k}:
-              </Text>
-              <Text variant={"label-large"} textTransform={"capitalize"}>
-                {traits[k]}
-              </Text>
-            </Flexbox>
+            <Text key={i} variant={"body-small"} textTransform={"capitalize"}>
+              {k}: {traits[k]}
+            </Text>
           ))}
       </Flexbox>
     </Box>
