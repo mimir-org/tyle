97d32e731652277271c0707f33671b90d20a26f2
Kai Andre Hovden <kai.hovden@bouvet.no>
2022-06-22 14:51:56 +0200

    #73893 Fixed Node soft delete endpoint in Tyle

--- src/server/TypeLibrary.Data/Contracts/Ef/IEfNodeRepository.cs
+++ src/server/TypeLibrary.Data/Contracts/Ef/IEfNodeRepository.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using System.Threading.Tasks;
 using Mimirorg.Common.Abstract;
 using TypeLibrary.Data.Models;
 
@@ -6,6 +7,7 @@ namespace TypeLibrary.Data.Contracts.Ef
 {
     public interface IEfNodeRepository : IGenericRepository<TypeLibraryDbContext, NodeLibDm>
     {
+        Task<NodeLibDm> Get(string id);
         IQueryable<NodeLibDm> GetAllNodes();
         IQueryable<NodeLibDm> FindNode(string id);
     }
