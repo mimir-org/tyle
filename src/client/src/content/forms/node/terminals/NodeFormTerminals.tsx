8a22e5a0e80472ba6a8ba91ef3c5f6c445892f02
Olav Gjerde <fruithut@tutanota.com>
2022-06-24 15:09:59 +0200

    [CLIENT] Adds attribute listing to NodeFormTerminals

--- src/client/src/content/forms/node/terminals/NodeFormTerminals.tsx
+++ src/client/src/content/forms/node/terminals/NodeFormTerminals.tsx
@@ -4,12 +4,16 @@ import { useTheme } from "styled-components/macro";
 import textResources from "../../../../assets/text/TextResources";
 import { Button } from "../../../../complib/buttons";
 import { Input, Select } from "../../../../complib/inputs";
-import { Box, Flexbox } from "../../../../complib/layouts";
+import { Box, Flexbox, Grid } from "../../../../complib/layouts";
 import { Text } from "../../../../complib/text";
 import { useGetTerminals } from "../../../../data/queries/tyle/queriesTerminal";
 import { createEmptyNodeTerminalLibAm } from "../../../../models/tyle/application/nodeTerminalLibAm";
 import { FormNodeLib } from "../../types/formNodeLib";
 import { connectorDirectionOptions, onTerminalAmountChange } from "./NodeFormTerminals.helpers";
+import { TerminalButton } from "../../../home/components/about/components/terminal/TerminalButton";
+import { Fragment } from "react";
+import { AttributeInfoButton } from "../../../home/components/about/components/attribute/AttributeInfoButton";
+import { mapAttributeLibCmToAttributeItem } from "../../../../utils/mappers";
 
 export interface NodeFormTerminalsProps {
   control: Control<FormNodeLib>;
@@ -37,56 +41,82 @@ export const NodeFormTerminals = ({ control }: NodeFormTerminalsProps) => {
         </Button>
       </Flexbox>
 
-      <Flexbox flexDirection={"column"} gap={theme.tyle.spacing.xl}>
-        {terminalFields.fields.map((field, index) => (
-          <Flexbox key={field.id} gap={theme.tyle.spacing.l} flexWrap={"wrap"}>
-            <Controller
-              control={control}
-              name={`nodeTerminals.${index}.terminalId`}
-              render={({ field: { value, onChange, ref, ...rest } }) => (
-                <Select
-                  {...rest}
-                  selectRef={ref}
-                  placeholder={textResources.TERMINAL_PLACEHOLDER}
-                  options={terminalQuery.data}
-                  isLoading={terminalQuery.isLoading}
-                  getOptionLabel={(x) => x.name}
-                  getOptionValue={(x) => x.id}
-                  onChange={(x) => onChange(x?.id)}
-                  value={terminalQuery.data?.find((x) => x.id === value)}
-                />
-              )}
-            />
-            <Controller
-              control={control}
-              name={`nodeTerminals.${index}.connectorDirection`}
-              render={({ field: { value, onChange, ref, ...rest } }) => (
-                <Select
-                  {...rest}
-                  selectRef={ref}
-                  placeholder={textResources.TERMINAL_DIRECTION_PLACEHOLDER}
-                  options={connectorDirectionOptions}
-                  getOptionLabel={(x) => x.label}
-                  onChange={(x) => onChange(x?.value)}
-                  value={connectorDirectionOptions.find((x) => x.value === value)}
-                />
-              )}
-            />
-            <Controller
-              control={control}
-              name={`nodeTerminals.${index}.quantity`}
-              render={({ field: { onChange, value } }) => (
-                <Input
-                  {...field}
-                  type="number"
-                  value={value}
-                  onChange={(e) => onTerminalAmountChange(index, e, terminalFields.remove, onChange)}
-                />
-              )}
-            />
-          </Flexbox>
-        ))}
-      </Flexbox>
+      <Grid
+        gridTemplateColumns={"250px 120px 250px 1fr"}
+        columnGap={theme.tyle.spacing.xxxl}
+        rowGap={theme.tyle.spacing.l}
+      >
+        <Text variant={"title-medium"}>{textResources.TERMINAL_TABLE_NAME}</Text>
+        <div />
+        <Text variant={"title-medium"}>{textResources.TERMINAL_TABLE_DIRECTION}</Text>
+        <Text variant={"title-medium"} pl={theme.tyle.spacing.xxxl}>
+          {textResources.TERMINAL_TABLE_ATTRIBUTES}
+        </Text>
+
+        {terminalFields.fields.map((field, index) => {
+          const targetTerminal = terminalQuery.data?.find((x) => x.id === terminalFields.fields[index].terminalId);
+
+          return (
+            <Fragment key={index}>
+              <Controller
+                control={control}
+                name={`nodeTerminals.${index}.terminalId`}
+                render={({ field: { value, onChange, ref, ...rest } }) => (
+                  <Select
+                    {...rest}
+                    selectRef={ref}
+                    placeholder={textResources.TERMINAL_PLACEHOLDER}
+                    options={terminalQuery.data}
+                    isLoading={terminalQuery.isLoading}
+                    getOptionLabel={(x) => x.name}
+                    getOptionValue={(x) => x.id}
+                    onChange={(x) => onChange(x?.id)}
+                    value={terminalQuery.data?.find((x) => x.id === value)}
+                    formatOptionLabel={(x) => (
+                      <Flexbox alignItems={"center"} gap={theme.tyle.spacing.base}>
+                        {x.color && <TerminalButton as={"span"} variant={"small"} {...x} />}
+                        <Text>{x.name}</Text>
+                      </Flexbox>
+                    )}
+                  />
+                )}
+              />
+              <Controller
+                control={control}
+                name={`nodeTerminals.${index}.quantity`}
+                render={({ field: { onChange, value } }) => (
+                  <Input
+                    {...field}
+                    type="number"
+                    value={value}
+                    onChange={(e) => onTerminalAmountChange(index, e, terminalFields.remove, onChange)}
+                  />
+                )}
+              />
+              <Controller
+                control={control}
+                name={`nodeTerminals.${index}.connectorDirection`}
+                render={({ field: { value, onChange, ref, ...rest } }) => (
+                  <Select
+                    {...rest}
+                    selectRef={ref}
+                    placeholder={textResources.TERMINAL_DIRECTION_PLACEHOLDER}
+                    options={connectorDirectionOptions}
+                    getOptionLabel={(x) => x.label}
+                    onChange={(x) => onChange(x?.value)}
+                    value={connectorDirectionOptions.find((x) => x.value === value)}
+                  />
+                )}
+              />
+              <Box display={"flex"} flexWrap={"wrap"} gap={theme.tyle.spacing.xl} pl={theme.tyle.spacing.xxxl}>
+                {targetTerminal?.attributes.map(
+                  (x) => x && <AttributeInfoButton key={x.id} {...mapAttributeLibCmToAttributeItem(x)} />
+                )}
+              </Box>
+            </Fragment>
+          );
+        })}
+      </Grid>
     </Box>
   );
 };
