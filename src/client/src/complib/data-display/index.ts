d054b35f863cec024d3c0e704ebe84fcc5d01970
Olav Gjerde <fruithut@tutanota.com>
2022-06-30 15:06:08 +0200

    [CLIENT] Adds Table component
    - Styled wrappers for table, thead, tbody, tr, th and td

--- src/client/src/complib/data-display/index.ts
+++ src/client/src/complib/data-display/index.ts
@@ -1,3 +1,4 @@
+export { Table, Tr, Thead, Th, Tbody, Td } from "./table/Table";
 export { Divider } from "./divider/Divider";
 export { Tooltip } from "./tooltip/Tooltip";
 export { Popover } from "./popover/Popover";
