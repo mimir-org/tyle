d9793061c81a5fed76e947e85181be6a53a50282
Olav Gjerde <fruithut@tutanota.com>
2022-07-08 10:06:52 +0200

    [CLIENT] Removes usage of legacy fakerjs api

--- src/client/src/utils/mocks/mockTerminalItem.ts
+++ src/client/src/utils/mocks/mockTerminalItem.ts
@@ -3,7 +3,7 @@ import { TerminalItem } from "../../content/home/types/TerminalItem";
 import { mockAttributeItem } from "./mockAttributeItem";
 
 export const mockTerminalItem = (): TerminalItem => ({
-  name: `Terminal ${faker.random.alpha({ count: 3, upcase: true })}`,
+  name: `Terminal ${faker.random.alpha({ count: 3, casing: "upper" })}`,
   color: faker.internet.color(),
   amount: parseInt(faker.random.numeric(1)),
   direction: faker.helpers.arrayElement(["Input", "Output", "Bidirectional"]),
