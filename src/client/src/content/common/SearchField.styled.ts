e8782c14e7ed9d87343a622fbd16e833b1548013
Olav Gjerde <fruithut@tutanota.com>
2022-07-18 14:02:48 +0200

    refactor(client/content): extracts SearchField component's styled icon dependency

---
+++ src/client/src/content/common/SearchField.styled.ts
@@ -0,0 +1,8 @@
+import { Search } from "@styled-icons/heroicons-outline";
+import styled from "styled-components/macro";
+
+export const SearchFieldIcon = styled(Search)`
+  path {
+    stroke-width: 1;
+  }
+`;
