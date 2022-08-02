7303906ce3a85d0a81b9aa8760a9c4236462ded6
Kai Andre Hovden <kai.hovden@bouvet.no>
2022-08-02 14:05:54 +0200

    #75870 Adjusted node (Am, Dm, profiles)

--- src/client/src/utils/mappers/mapNodeLibCmToNodeLibAm.ts
+++ src/client/src/utils/mappers/mapNodeLibCmToNodeLibAm.ts
@@ -1,10 +1,14 @@
 import { NodeLibAm, NodeLibCm } from "@mimirorg/typelibrary-types";
 import { mapNodeTerminalLibCmsToNodeTerminalLibAms } from "./mapNodeTerminalLibCmToNodeTerminalLibAm";
 
-export const mapNodeLibCmToNodeLibAm = (node: NodeLibCm): NodeLibAm => ({
-  ...node,
-  simpleIdList: node.simples?.map((x) => x.id),
-  attributeIdList: node.attributes?.map((x) => x.id),
-  nodeTerminals: mapNodeTerminalLibCmsToNodeTerminalLibAms(node.nodeTerminals),
-  parentId: node.parentIri,
-});
+export const mapNodeLibCmToNodeLibAm = (node: NodeLibCm): NodeLibAm => { 
+  const cm = {
+    ...node,
+    simpleIdList: node.simples?.map((x) => x.id),
+    attributeIdList: node.attributes?.map((x) => x.id),
+    nodeTerminals: mapNodeTerminalLibCmsToNodeTerminalLibAms(node.nodeTerminals),
+    parentId: node.parentIri
+  }
+  const {id, firstVersionId, version, ...am} = cm;
+  return am as NodeLibAm;
+};
