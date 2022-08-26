8e2fa3151771d3a21ef64153231dc78d8b951cd4
Olav Gjerde <fruithut@tutanota.com>
2022-08-26 14:50:25 +0200

    fix(client): corrects react hook binding of selectValues in AttributeFormValues

--- src/client/src/content/forms/attribute/values/AttributeFormValues.tsx
+++ src/client/src/content/forms/attribute/values/AttributeFormValues.tsx
@@ -16,23 +16,28 @@ export interface AttributeFormValuesProps {
 export const AttributeFormValues = ({ control }: AttributeFormValuesProps) => {
   const theme = useTheme();
   const { t } = useTranslation("translation", { keyPrefix: "values" });
-  const valueFields = useFieldArray({ control, name: "selectValues" });
-  const onRemove = (index: number) => valueFields.remove(index);
+  const selectValueFields = useFieldArray({ control, name: "selectValues" });
+  const onRemove = (index: number) => selectValueFields.remove(index);
 
   return (
     <FormSection
       title={t("title")}
-      action={<FormAddButton buttonText={t("add")} onClick={() => valueFields.append({ value: "" })} />}
+      action={<FormAddButton buttonText={t("add")} onClick={() => selectValueFields.append({ value: "" })} />}
     >
       <Flexbox flexWrap={"wrap"} gap={theme.tyle.spacing.xl}>
-        {valueFields.fields.map((field, index) => (
-          <FieldsCard key={field.id} index={index + 1} removeText={t("remove")} onRemove={() => onRemove(index)}>
+        {selectValueFields.fields.map((selectValueField, index) => (
+          <FieldsCard
+            key={selectValueField.id}
+            index={index + 1}
+            removeText={t("remove")}
+            onRemove={() => onRemove(index)}
+          >
             <Controller
               control={control}
-              name={`selectValues.${index}`}
-              render={({ field: { value, ...rest } }) => (
+              name={`selectValues.${index}.value`}
+              render={({ field }) => (
                 <FormField label={t("value")}>
-                  <Input {...rest} id={field.id} value={value.value} placeholder={t("placeholders.value")} />
+                  <Input {...field} id={selectValueField.id} placeholder={t("placeholders.value")} />
                 </FormField>
               )}
             />
