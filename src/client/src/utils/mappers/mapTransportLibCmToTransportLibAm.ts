e38409ac57f41f0b74bfe7c62705fb928bb5efd9
Olav Gjerde <fruithut@tutanota.com>
2022-09-07 12:48:08 +0200

    fix(client): corrects parentId mapping for transports

--- src/client/src/utils/mappers/mapTransportLibCmToTransportLibAm.ts
+++ src/client/src/utils/mappers/mapTransportLibCmToTransportLibAm.ts
@@ -3,7 +3,5 @@ import { TransportLibAm, TransportLibCm } from "@mimirorg/typelibrary-types";
 export const mapTransportLibCmToTransportLibAm = (transport: TransportLibCm): TransportLibAm => ({
   ...transport,
   attributeIdList: transport.attributes.map((x) => x.id),
-
-  //TODO: Verify relation
-  parentId: transport.parentIri,
+  parentId: transport.parentId,
 });
