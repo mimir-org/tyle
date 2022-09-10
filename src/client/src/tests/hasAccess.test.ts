a44ff7907c8acd47c7d3435b2943bdf75e682f6c
Reidar Liabø <RLIAB@equinor.com>
2022-09-11 00:24:21 +0200

    HasAccess implementation

--- src/client/src/tests/hasAccess.test.ts
+++ src/client/src/tests/hasAccess.test.ts
@@ -1,97 +1,133 @@
 import { hasAccess } from "../utils/hasAccess";
 import { MimirorgPermission, MimirorgUserCm } from "@mimirorg/typelibrary-types";
 
-test("manager has correct accesses", () => {
-  const user = {
-    id: "12345",
-    firstName: "Hans",
-    lastName: "Hasen",
-    email: "hans.hansen@runir.net",
-    phoneNumber: "12345678",
-    permissions: {
-      "1": MimirorgPermission.Manage,
-    },
-  } as MimirorgUserCm;
+describe("hasAccess tests", () => {
+  test("undefined user returns false", () => {
+    expect(hasAccess({} as MimirorgUserCm, 0, MimirorgPermission.Manage)).toBe(false);
+  });
 
-  expect(hasAccess(user, 1, MimirorgPermission.Manage)).toBe(true);
-  expect(hasAccess(user, 1, MimirorgPermission.Approve)).toBe(true);
-  expect(hasAccess(user, 1, MimirorgPermission.Delete)).toBe(true);
-  expect(hasAccess(user, 1, MimirorgPermission.Write)).toBe(true);
-  expect(hasAccess(user, 1, MimirorgPermission.Read)).toBe(true);
-});
+  test("missing company id returns false", () => {
+    const user = {
+      id: "12345",
+      firstName: "Hans",
+      lastName: "Hasen",
+      email: "hans.hansen@runir.net",
+      phoneNumber: "12345678",
+      permissions: {},
+    } as MimirorgUserCm;
 
-test("approver has correct accesses", () => {
-  const user = {
-    id: "12345",
-    firstName: "Hans",
-    lastName: "Hasen",
-    email: "hans.hansen@runir.net",
-    phoneNumber: "12345678",
-    permissions: {
-      "1": MimirorgPermission.Approve,
-    },
-  } as MimirorgUserCm;
+    expect(hasAccess(user, 0, MimirorgPermission.Manage)).toBe(false);
+  });
 
-  expect(hasAccess(user, 1, MimirorgPermission.Manage)).toBe(false);
-  expect(hasAccess(user, 1, MimirorgPermission.Approve)).toBe(true);
-  expect(hasAccess(user, 1, MimirorgPermission.Delete)).toBe(true);
-  expect(hasAccess(user, 1, MimirorgPermission.Write)).toBe(true);
-  expect(hasAccess(user, 1, MimirorgPermission.Read)).toBe(true);
-});
+  test("empty permissions validates ok", () => {
+    const user = {
+      id: "12345",
+      firstName: "Hans",
+      lastName: "Hasen",
+      email: "hans.hansen@runir.net",
+      phoneNumber: "12345678",
+      permissions: {},
+    } as MimirorgUserCm;
 
-test("delete has correct accesses", () => {
-  const user = {
-    id: "12345",
-    firstName: "Hans",
-    lastName: "Hasen",
-    email: "hans.hansen@runir.net",
-    phoneNumber: "12345678",
-    permissions: {
-      "1": MimirorgPermission.Delete,
-    },
-  } as MimirorgUserCm;
+    expect(hasAccess(user, 1, MimirorgPermission.Manage)).toBe(false);
+    expect(hasAccess(user, 1, MimirorgPermission.Approve)).toBe(false);
+    expect(hasAccess(user, 1, MimirorgPermission.Delete)).toBe(false);
+    expect(hasAccess(user, 1, MimirorgPermission.Write)).toBe(false);
+    expect(hasAccess(user, 1, MimirorgPermission.Read)).toBe(false);
+  });
 
-  expect(hasAccess(user, 1, MimirorgPermission.Manage)).toBe(false);
-  expect(hasAccess(user, 1, MimirorgPermission.Approve)).toBe(false);
-  expect(hasAccess(user, 1, MimirorgPermission.Delete)).toBe(true);
-  expect(hasAccess(user, 1, MimirorgPermission.Write)).toBe(true);
-  expect(hasAccess(user, 1, MimirorgPermission.Read)).toBe(true);
-});
+  test("manage validates ok", () => {
+    const user = {
+      id: "12345",
+      firstName: "Hans",
+      lastName: "Hasen",
+      email: "hans.hansen@runir.net",
+      phoneNumber: "12345678",
+      permissions: {
+        "1": MimirorgPermission.Manage,
+      },
+    } as MimirorgUserCm;
 
-test("write has correct accesses", () => {
-  const user = {
-    id: "12345",
-    firstName: "Hans",
-    lastName: "Hasen",
-    email: "hans.hansen@runir.net",
-    phoneNumber: "12345678",
-    permissions: {
-      "1": MimirorgPermission.Write,
-    },
-  } as MimirorgUserCm;
+    expect(hasAccess(user, 1, MimirorgPermission.Manage)).toBe(true);
+    expect(hasAccess(user, 1, MimirorgPermission.Approve)).toBe(true);
+    expect(hasAccess(user, 1, MimirorgPermission.Delete)).toBe(true);
+    expect(hasAccess(user, 1, MimirorgPermission.Write)).toBe(true);
+    expect(hasAccess(user, 1, MimirorgPermission.Read)).toBe(true);
+  });
 
-  expect(hasAccess(user, 1, MimirorgPermission.Manage)).toBe(false);
-  expect(hasAccess(user, 1, MimirorgPermission.Approve)).toBe(false);
-  expect(hasAccess(user, 1, MimirorgPermission.Delete)).toBe(false);
-  expect(hasAccess(user, 1, MimirorgPermission.Write)).toBe(true);
-  expect(hasAccess(user, 1, MimirorgPermission.Read)).toBe(true);
-});
+  test("approve validates ok", () => {
+    const user = {
+      id: "12345",
+      firstName: "Hans",
+      lastName: "Hasen",
+      email: "hans.hansen@runir.net",
+      phoneNumber: "12345678",
+      permissions: {
+        "1": MimirorgPermission.Approve,
+      },
+    } as MimirorgUserCm;
 
-test("read has correct accesses", () => {
-  const user = {
-    id: "12345",
-    firstName: "Hans",
-    lastName: "Hasen",
-    email: "hans.hansen@runir.net",
-    phoneNumber: "12345678",
-    permissions: {
-      "1": MimirorgPermission.Read,
-    },
-  } as MimirorgUserCm;
+    expect(hasAccess(user, 1, MimirorgPermission.Manage)).toBe(false);
+    expect(hasAccess(user, 1, MimirorgPermission.Approve)).toBe(true);
+    expect(hasAccess(user, 1, MimirorgPermission.Delete)).toBe(true);
+    expect(hasAccess(user, 1, MimirorgPermission.Write)).toBe(true);
+    expect(hasAccess(user, 1, MimirorgPermission.Read)).toBe(true);
+  });
 
-  expect(hasAccess(user, 1, MimirorgPermission.Manage)).toBe(false);
-  expect(hasAccess(user, 1, MimirorgPermission.Approve)).toBe(false);
-  expect(hasAccess(user, 1, MimirorgPermission.Delete)).toBe(false);
-  expect(hasAccess(user, 1, MimirorgPermission.Write)).toBe(false);
-  expect(hasAccess(user, 1, MimirorgPermission.Read)).toBe(true);
+  test("delete validates ok", () => {
+    const user = {
+      id: "12345",
+      firstName: "Hans",
+      lastName: "Hasen",
+      email: "hans.hansen@runir.net",
+      phoneNumber: "12345678",
+      permissions: {
+        "1": MimirorgPermission.Delete,
+      },
+    } as MimirorgUserCm;
+
+    expect(hasAccess(user, 1, MimirorgPermission.Manage)).toBe(false);
+    expect(hasAccess(user, 1, MimirorgPermission.Approve)).toBe(false);
+    expect(hasAccess(user, 1, MimirorgPermission.Delete)).toBe(true);
+    expect(hasAccess(user, 1, MimirorgPermission.Write)).toBe(true);
+    expect(hasAccess(user, 1, MimirorgPermission.Read)).toBe(true);
+  });
+
+  test("write validates ok", () => {
+    const user = {
+      id: "12345",
+      firstName: "Hans",
+      lastName: "Hasen",
+      email: "hans.hansen@runir.net",
+      phoneNumber: "12345678",
+      permissions: {
+        "1": MimirorgPermission.Write,
+      },
+    } as MimirorgUserCm;
+
+    expect(hasAccess(user, 1, MimirorgPermission.Manage)).toBe(false);
+    expect(hasAccess(user, 1, MimirorgPermission.Approve)).toBe(false);
+    expect(hasAccess(user, 1, MimirorgPermission.Delete)).toBe(false);
+    expect(hasAccess(user, 1, MimirorgPermission.Write)).toBe(true);
+    expect(hasAccess(user, 1, MimirorgPermission.Read)).toBe(true);
+  });
+
+  test("read validates ok", () => {
+    const user = {
+      id: "12345",
+      firstName: "Hans",
+      lastName: "Hasen",
+      email: "hans.hansen@runir.net",
+      phoneNumber: "12345678",
+      permissions: {
+        "1": MimirorgPermission.Read,
+      },
+    } as MimirorgUserCm;
+
+    expect(hasAccess(user, 1, MimirorgPermission.Manage)).toBe(false);
+    expect(hasAccess(user, 1, MimirorgPermission.Approve)).toBe(false);
+    expect(hasAccess(user, 1, MimirorgPermission.Delete)).toBe(false);
+    expect(hasAccess(user, 1, MimirorgPermission.Write)).toBe(false);
+    expect(hasAccess(user, 1, MimirorgPermission.Read)).toBe(true);
+  });
 });
--- src/client/src/utils/hasAccess.ts
+++ src/client/src/utils/hasAccess.ts
@@ -1,8 +1,8 @@
 import { MimirorgPermission, MimirorgUserCm } from "@mimirorg/typelibrary-types";
 
-export const enumToBitValues = (enumValue: object): number[] => Object.keys(enumValue).map(Number).filter(Boolean);
-
 export const hasAccess = (user: MimirorgUserCm, company: number, permission: MimirorgPermission): boolean => {
+  if (!user) return false;
+  if (company < 1) return false;
   const companyPermission = user?.permissions[company] && (user?.permissions[company] as MimirorgPermission);
   if ((companyPermission & permission) === permission) return true;
   return false;
