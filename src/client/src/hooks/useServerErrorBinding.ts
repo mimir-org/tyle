965d65cb9bb3bf2850ca3c12343475ecbac8989e
Olav Gjerde <fruithut@tutanota.com>
2022-09-12 16:23:58 +0200

    fix(client): corrects error name binding by formatting server errors with camelCasing

--- src/client/src/hooks/useServerErrorBinding.ts
+++ src/client/src/hooks/useServerErrorBinding.ts
@@ -1,8 +1,10 @@
+import { camelCase } from "lodash";
 import { useEffect } from "react";
 import { FieldValues, Path, UseFormSetError } from "react-hook-form";
 
 /**
  * If any errors present it will combine errors received with the error structure in react-hook-form
+ *
  * @param setError function with puts a given error into a structure where it can be consumed
  * @param errors to be placed in react-hook-form error-structure
  */
@@ -13,7 +15,7 @@ export function useValidationFromServer<T extends FieldValues>(
   useEffect(() => {
     if (errors) {
       Object.keys(errors).forEach((propertyName) => {
-        setError(propertyName as Path<T>, {
+        setError(camelCase(propertyName) as Path<T>, {
           type: "server",
           message: errors[propertyName as keyof T].join(". "),
         });
