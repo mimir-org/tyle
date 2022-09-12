ede43065fc7b2849c9bc283abe371f561a010785
Olav Gjerde <fruithut@tutanota.com>
2022-09-12 09:59:52 +0200

    refactor(client): simplifies hasAccess check

--- src/client/src/utils/hasAccess.ts
+++ src/client/src/utils/hasAccess.ts
@@ -1,4 +1,4 @@
-import { MimirorgPermission, MimirorgUserCm, MimirorgCompanyCm } from "@mimirorg/typelibrary-types";
+import { MimirorgCompanyCm, MimirorgPermission, MimirorgUserCm } from "@mimirorg/typelibrary-types";
 
 /**
  * Check if user has access to a spesific company with given permission.
@@ -10,9 +10,9 @@ import { MimirorgPermission, MimirorgUserCm, MimirorgCompanyCm } from "@mimirorg
 export const hasAccess = (user: MimirorgUserCm, company: number, permission: MimirorgPermission): boolean => {
   if (!user) return false;
   if (company < 1) return false;
-  const companyPermission = user?.permissions[company] && (user?.permissions[company] as MimirorgPermission);
-  if ((companyPermission & permission) === permission) return true;
-  return false;
+  const companyPermission = user?.permissions[company];
+
+  return (companyPermission & permission) === permission;
 };
 
 /**
