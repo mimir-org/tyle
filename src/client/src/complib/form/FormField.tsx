cebda17a2c1b20c56b78efd463f68706a15521f2
Olav Gjerde <fruithut@tutanota.com>
2022-09-14 08:51:06 +0200

    refactor(client): adjusts FormField to not render label wrapper when no label is given

--- src/client/src/complib/form/FormField.tsx
+++ src/client/src/complib/form/FormField.tsx
@@ -2,6 +2,7 @@ import { PropsWithChildren } from "react";
 import { useTheme } from "styled-components";
 import { Box, Flexbox, MotionFlexbox } from "../layouts";
 import { MotionText, Text } from "../text";
+import { ConditionalWrapper } from "../utils";
 
 interface FormFieldProps {
   label?: string;
@@ -9,7 +10,8 @@ interface FormFieldProps {
 }
 
 /**
- * A component for wrapping form inputs with a label and an error message
+ * A component for wrapping form inputs with an error message and/or a label
+ *
  * @param label describing the input
  * @param error message for the given input
  * @param children
@@ -17,22 +19,30 @@ interface FormFieldProps {
  */
 export const FormField = ({ label, error, children }: PropsWithChildren<FormFieldProps>) => {
   const theme = useTheme();
+  const hasLabel = !!label?.length;
 
   return (
     <MotionFlexbox layout flexDirection={"column"} gap={theme.tyle.spacing.s}>
-      <Flexbox as={"label"} flexDirection={"column"} gap={theme.tyle.spacing.xs}>
-        <Box borderLeft={"1px solid transparent"}>
-          <Text
-            as={"span"}
-            variant={"label-large"}
-            color={theme.tyle.color.sys.surface.variant.on}
-            pl={theme.tyle.spacing.l}
-          >
-            {label}
-          </Text>
-        </Box>
-        {children}
-      </Flexbox>
+      <ConditionalWrapper
+        condition={hasLabel}
+        wrapper={(c) => (
+          <Flexbox as={"label"} flexDirection={"column"} gap={theme.tyle.spacing.xs}>
+            <Box borderLeft={"1px solid transparent"}>
+              <Text
+                as={"span"}
+                variant={"label-large"}
+                color={theme.tyle.color.sys.surface.variant.on}
+                pl={theme.tyle.spacing.l}
+              >
+                {label}
+              </Text>
+            </Box>
+            {c}
+          </Flexbox>
+        )}
+      >
+        <>{children}</>
+      </ConditionalWrapper>
 
       {error && error.message && (
         <MotionText variant={"label-medium"} color={theme.tyle.color.sys.error.base} {...theme.tyle.animation.fade}>
