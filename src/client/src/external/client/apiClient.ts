4021d8fa95af7c6acd4f7b6a50f58a39aa7c0022
Olav Gjerde <fruithut@tutanota.com>
2022-10-31 12:44:18 +0100

    refactor(client/external): converts all relative paths to absolute

--- src/client/src/external/client/apiClient.ts
+++ src/client/src/external/client/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import config from "common/utils/config";
-import { injectBearerToken } from "./interceptors/injectBearerToken";
-import { refreshExpiredToken } from "./interceptors/refreshExpiredToken";
+import { injectBearerToken } from "external/client/interceptors/injectBearerToken";
+import { refreshExpiredToken } from "external/client/interceptors/refreshExpiredToken";
 
 export const apiClient = axios.create({
   baseURL: config.API_BASE_URL,
