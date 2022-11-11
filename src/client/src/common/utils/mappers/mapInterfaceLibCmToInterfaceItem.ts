f37b68f2bff482adfbbce8206310ced1bb49c110
Olav Gjerde <fruithut@tutanota.com>
2022-11-11 14:48:52 +0100

    feat(client): extends item mappers with state information

--- src/client/src/common/utils/mappers/mapInterfaceLibCmToInterfaceItem.ts
+++ src/client/src/common/utils/mappers/mapInterfaceLibCmToInterfaceItem.ts
@@ -1,18 +1,30 @@
-import { InterfaceLibCm } from "@mimirorg/typelibrary-types";
+import { InterfaceLibCm, State } from "@mimirorg/typelibrary-types";
 import { InterfaceItem } from "common/types/interfaceItem";
 import { getColorFromAspect } from "common/utils/getColorFromAspect";
+import { getValueLabelObjectsFromEnum } from "common/utils/getValueLabelObjectsFromEnum";
 import { mapAttributeLibCmsToInfoItems } from "common/utils/mappers/mapAttributeLibCmToInfoItem";
 import { mapTerminalLibCmToTerminalItem } from "common/utils/mappers/mapTerminalLibCmToTerminalItem";
 import { sortInfoItems } from "common/utils/sorters";
 
-export const mapInterfaceLibCmToInterfaceItem = (interfaceLibCm: InterfaceLibCm): InterfaceItem => ({
-  id: interfaceLibCm.id,
-  name: interfaceLibCm.name,
-  description: interfaceLibCm.description,
-  aspectColor: getColorFromAspect(interfaceLibCm.aspect),
-  interfaceColor: interfaceLibCm.terminal.color,
-  attributes: sortInfoItems(mapAttributeLibCmsToInfoItems(interfaceLibCm.attributes)),
-  terminal: mapTerminalLibCmToTerminalItem(interfaceLibCm.terminal),
-  tokens: [interfaceLibCm.version, interfaceLibCm.companyName, interfaceLibCm.rdsName, interfaceLibCm.purposeName],
-  kind: "InterfaceItem",
-});
+export const mapInterfaceLibCmToInterfaceItem = (interfaceLibCm: InterfaceLibCm): InterfaceItem => {
+  const states = getValueLabelObjectsFromEnum(State);
+  const currentStateLabel = states[interfaceLibCm.state].label;
+
+  return {
+    id: interfaceLibCm.id,
+    name: interfaceLibCm.name,
+    description: interfaceLibCm.description,
+    aspectColor: getColorFromAspect(interfaceLibCm.aspect),
+    interfaceColor: interfaceLibCm.terminal.color,
+    attributes: sortInfoItems(mapAttributeLibCmsToInfoItems(interfaceLibCm.attributes)),
+    terminal: mapTerminalLibCmToTerminalItem(interfaceLibCm.terminal),
+    tokens: [
+      interfaceLibCm.version,
+      interfaceLibCm.companyName,
+      currentStateLabel,
+      interfaceLibCm.rdsName,
+      interfaceLibCm.purposeName,
+    ],
+    kind: "InterfaceItem",
+  };
+};
--- src/client/src/common/utils/mappers/mapTerminalLibCmToTerminalItem.ts
+++ src/client/src/common/utils/mappers/mapTerminalLibCmToTerminalItem.ts
@@ -1,14 +1,20 @@
-import { TerminalLibCm } from "@mimirorg/typelibrary-types";
+import { State, TerminalLibCm } from "@mimirorg/typelibrary-types";
 import { TerminalItem } from "common/types/terminalItem";
+import { getValueLabelObjectsFromEnum } from "common/utils/getValueLabelObjectsFromEnum";
 import { mapAttributeLibCmsToInfoItems } from "common/utils/mappers/mapAttributeLibCmToInfoItem";
 import { sortInfoItems } from "common/utils/sorters";
 
-export const mapTerminalLibCmToTerminalItem = (terminal: TerminalLibCm): TerminalItem => ({
-  id: terminal.id,
-  name: terminal.name,
-  description: terminal.description,
-  color: terminal.color,
-  attributes: sortInfoItems(mapAttributeLibCmsToInfoItems(terminal.attributes)),
-  tokens: [terminal.version, terminal.companyName],
-  kind: "TerminalItem",
-});
+export const mapTerminalLibCmToTerminalItem = (terminal: TerminalLibCm): TerminalItem => {
+  const states = getValueLabelObjectsFromEnum(State);
+  const currentStateLabel = states[terminal.state].label;
+
+  return {
+    id: terminal.id,
+    name: terminal.name,
+    description: terminal.description,
+    color: terminal.color,
+    attributes: sortInfoItems(mapAttributeLibCmsToInfoItems(terminal.attributes)),
+    tokens: [terminal.version, terminal.companyName, currentStateLabel],
+    kind: "TerminalItem",
+  };
+};
