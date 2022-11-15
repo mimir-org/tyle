50ab2695e887de80f6a436ccea39842472a03d21
Olav Gjerde <fruithut@tutanota.com>
2022-11-15 13:59:01 +0100

    refactor(client): moves link type to common types

--- src/client/src/features/explore/search/Search.helpers.ts
+++ src/client/src/features/explore/search/Search.helpers.ts
@@ -1,8 +1,8 @@
+import { Link } from "common/types/link";
 import { interfaceFormBasePath } from "features/entities/interface/InterfaceFormRoutes";
 import { nodeFormBasePath } from "features/entities/node/NodeFormRoutes";
 import { terminalFormBasePath } from "features/entities/terminal/TerminalFormRoutes";
 import { transportFormBasePath } from "features/entities/transport/TransportFormRoutes";
-import { Link } from "features/explore/search/types/link";
 
 export const getCreateMenuLinks = (): Link[] => {
   return [
