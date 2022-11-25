06e116f807a04beaaa45112da24d082552762e06
Olav Gjerde <fruithut@tutanota.com>
2022-11-25 10:34:19 +0100

    feat(client/complib): extends alert dialog and dialog with controlled mode props

--- src/client/src/complib/overlays/alert-dialog/AlertDialog.tsx
+++ src/client/src/complib/overlays/alert-dialog/AlertDialog.tsx
@@ -12,48 +12,54 @@ import { AlertDialogTitle } from "complib/overlays/alert-dialog/components/Alert
 import { PropsWithChildren, ReactNode } from "react";
 import { useTheme } from "styled-components";
 
-type AlertDialogProps = AlertDialogContentProps & {
-  content?: ReactNode;
-  actions: AlertDialogActionItem[];
-  title: string;
-  description?: string;
-  hideTitle?: boolean;
-  hideDescription?: boolean;
-  cancelText?: string;
-};
+type AlertDialogProps = Pick<AlertDialogPrimitive.AlertDialogProps, "open" | "onOpenChange"> &
+  AlertDialogContentProps & {
+    content?: ReactNode;
+    actions: AlertDialogActionItem[];
+    title: string;
+    description?: string;
+    hideTitle?: boolean;
+    hideDescription?: boolean;
+    cancelText?: string;
+  };
 
 /**
- * Component that interrupts the user with important content and expects a response
+ * Component that interrupts the user with important content and expects a response.
+ * Can operate in both a controlled and uncontrolled mode by utilizing open and onOpenChange properties.
  *
  * See documentation link below for details.
  * @see https://www.radix-ui.com/docs/primitives/components/alert-dialog
  *
  * @param children component that triggers dialog visibility
  * @param content shown inside the dialog itself
+ * @param open property for overriding the open state of the dialog
+ * @param onOpenChange event handler called when the open state of the dialog changes
  * @param actions what actions the user can take
+ * @param cancelAction property for overriding the cancel action's text and click handler
  * @param title required title of dialog (can be hidden visually with hideTitle prop)
  * @param description optional description of dialog
  * @param hideTitle hides the title from view while remaining readable by screen-readers
  * @param hideDescription hides the description from view while remaining readable by screen-readers
- * @param cancelText property for overriding the text of the cancel action
  * @param delegated receives sizing and flexbox props for overriding default styles
  * @constructor
  */
 export const AlertDialog = ({
   children,
   content,
+  open,
+  onOpenChange,
   actions,
+  cancelAction,
   title,
   hideTitle,
   description,
   hideDescription,
-  cancelText,
   ...delegated
 }: PropsWithChildren<AlertDialogProps>) => {
   const theme = useTheme();
 
   return (
-    <AlertDialogPrimitive.Root>
+    <AlertDialogPrimitive.Root open={open} onOpenChange={onOpenChange}>
       <AlertDialogPrimitive.Trigger asChild>{children}</AlertDialogPrimitive.Trigger>
       <AlertDialogPrimitive.Portal>
         <AlertDialogPrimitive.Overlay asChild>
