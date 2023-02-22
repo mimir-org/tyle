9b5357cdae95e7ae0484384765f0add6168013ed
Jørgen Drejer Norheim <jorgen.norheim@bouvet.no>
2023-02-22 10:02:19 +0100

    Removed Interface and Transport related code from ApprovalService

--- src/server/TypeLibrary.Services/Services/ApprovalService.cs
+++ src/server/TypeLibrary.Services/Services/ApprovalService.cs
@@ -12,17 +12,13 @@ namespace TypeLibrary.Services.Services
     {
         private readonly IMapper _mapper;
         private readonly INodeService _nodeService;
-        private readonly ITransportService _transportService;
-        private readonly IInterfaceService _interfaceService;
         private readonly ITerminalService _terminalService;
         private readonly IMimirorgAuthService _authService;
 
-        public ApprovalService(IMapper mapper, INodeService nodeService, ITransportService transportService, IInterfaceService interfaceService, ITerminalService terminalService, IMimirorgAuthService authService)
+        public ApprovalService(IMapper mapper, INodeService nodeService, ITerminalService terminalService, IMimirorgAuthService authService)
         {
             _mapper = mapper;
             _nodeService = nodeService;
-            _transportService = transportService;
-            _interfaceService = interfaceService;
             _terminalService = terminalService;
             _authService = authService;
         }
@@ -38,8 +34,6 @@ namespace TypeLibrary.Services.Services
             var tasks = new List<Task>
             {
                 Task.Run(() => data.ResolveNodes(_nodeService, _mapper, _authService)),
-                Task.Run(() => data.ResolveTransports(_transportService, _mapper, _authService)),
-                Task.Run(() => data.ResolveInterfaces(_interfaceService, _mapper, _authService)),
                 Task.Run(() => data.ResolveTerminals(_terminalService, _mapper, _authService))
             };
 
