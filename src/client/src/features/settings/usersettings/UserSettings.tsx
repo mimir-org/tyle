3cccf70ade237716aca6ee9c4949c8f4b7bf8d81
Jørgen Drejer Norheim <jorgen.norheim@bouvet.no>
2023-03-06 09:41:08 +0100

    Fix (client): Removed unused variable

--- src/client/src/features/settings/usersettings/UserSettings.tsx
+++ src/client/src/features/settings/usersettings/UserSettings.tsx
@@ -2,8 +2,6 @@ import { SettingsSection } from "../common/settings-section/SettingsSection";
 import { useTranslation } from "react-i18next";
 import { UserSettingsForm } from "features/settings/usersettings/UserSettingsForm";
 
-const _basePath = "mimirorguser";
-
 export const UserSettings = () => {
   const { t } = useTranslation("settings");
 
