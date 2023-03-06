6afbb9b58866067c494db0046397c4d077674a17
Jørgen Drejer Norheim <jorgen.norheim@bouvet.no>
2023-03-06 10:16:24 +0100

    Added a empty form for creating a new company

--- src/client/src/features/settings/company/CreateCompany.tsx
+++ src/client/src/features/settings/company/CreateCompany.tsx
@@ -1,12 +1,13 @@
 import { SettingsSection } from "../common/settings-section/SettingsSection";
 import { useTranslation } from "react-i18next";
+import { CreateCompanyForm } from "features/settings/company/CreateCompanyForm";
 
 export const CreateCompany = () => {
   const { t } = useTranslation("settings");
 
   return (
     <SettingsSection title={t("createCompany.title")}>
-      
+      <CreateCompanyForm />
     </SettingsSection>
   );
 };
---
+++ src/client/src/features/settings/company/CreateCompanyForm.tsx
@@ -0,0 +1,12 @@
+import { Form } from "complib/form";
+import { useTranslation } from "react-i18next";
+
+export const CreateCompanyForm = () => {
+  const { t } = useTranslation("settings");
+
+  return (
+    <Form>
+        
+    </Form>
+  );
+};
