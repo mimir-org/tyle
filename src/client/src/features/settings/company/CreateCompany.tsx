8bdcf1722165b4ea36ab1286938cbf0ff199206f
Jørgen Drejer Norheim <jorgen.norheim@bouvet.no>
2023-03-07 08:39:52 +0100

    Made the create company form wider

--- src/client/src/features/settings/company/CreateCompany.tsx
+++ src/client/src/features/settings/company/CreateCompany.tsx
@@ -1,13 +1,16 @@
 import { SettingsSection } from "../common/settings-section/SettingsSection";
 import { useTranslation } from "react-i18next";
 import { CreateCompanyForm } from "features/settings/company/CreateCompanyForm";
+import { Box } from "complib/layouts";
 
 export const CreateCompany = () => {
   const { t } = useTranslation("settings");
 
   return (
-    <SettingsSection title={t("createCompany.title")}>
-      <CreateCompanyForm />
-    </SettingsSection>
+    <Box minWidth={"60%"}>
+      <SettingsSection title={t("createCompany.title")}>
+        <CreateCompanyForm />
+      </SettingsSection>
+    </Box>
   );
 };
