4452e7d168d0c7d39dfaaaa4bcc0b3a8742dec48
Jørgen Drejer Norheim <jorgen.norheim@bouvet.no>
2023-03-15 15:24:24 +0100

    Fixed IRIs for Symbols

--- src/server/TypeLibrary.Data/Repositories/Ef/EfSymbolRepository.cs
+++ src/server/TypeLibrary.Data/Repositories/Ef/EfSymbolRepository.cs
@@ -1,8 +1,10 @@
 using System.Collections.Generic;
+using System.Runtime;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Mimirorg.Common.Abstract;
 using Mimirorg.Common.Enums;
+using TypeLibrary.Data.Contracts;
 using TypeLibrary.Data.Contracts.Ef;
 using TypeLibrary.Data.Models;
 
@@ -10,8 +12,11 @@ namespace TypeLibrary.Data.Repositories.Ef
 {
     public class EfSymbolRepository : GenericRepository<TypeLibraryDbContext, SymbolLibDm>, IEfSymbolRepository
     {
-        public EfSymbolRepository(TypeLibraryDbContext dbContext) : base(dbContext)
+        private readonly IApplicationSettingsRepository _settings;
+
+        public EfSymbolRepository(IApplicationSettingsRepository settings, TypeLibraryDbContext dbContext) : base(dbContext)
         {
+            _settings = settings;
         }
 
         public async Task<string> GetSymbolDataAsync(int id)
@@ -32,6 +37,9 @@ namespace TypeLibrary.Data.Repositories.Ef
 
             Attach(symbols, EntityState.Added);
             await SaveAsync();
+            foreach (var item in symbols)
+                item.Iri = $"{_settings.ApplicationSemanticUrl}/symbol/{item.Id}";
+            await SaveAsync();
             Detach(symbols);
         }
 
@@ -39,6 +47,8 @@ namespace TypeLibrary.Data.Repositories.Ef
         {
             symbol.State = state;
             Attach(symbol, EntityState.Added);
+            await SaveAsync();
+            symbol.Iri = $"{_settings.ApplicationSemanticUrl}/symbol/{symbol.Id}";
             await SaveAsync();
             Detach(symbol);
             return symbol;
