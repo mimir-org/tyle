fd273bf94466d2738b92df48fb8b775d48c2a7df
Jørgen Drejer Norheim <jorgen.norheim@bouvet.no>
2023-03-21 14:15:48 +0100

    Migration for new db structure

---
+++ src/server/TypeLibrary.Core/Migrations/20230321113138_AddAttributesUnits.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TypeLibrary.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAttributesUnits : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Attributes",
+                table: "Terminal");
+
+            migrationBuilder.DropColumn(
+                name: "Attributes",
+                table: "Node");
+
+            migrationBuilder.CreateTable(
+                name: "Attribute",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(127)", maxLength: 127, nullable: false),
+                    Iri = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    TypeReferences = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    State = table.Column<string>(type: "nvarchar(31)", maxLength: 31, nullable: false),
+                    CompanyId = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(511)", maxLength: 511, nullable: true),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(127)", maxLength: 127, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Attribute", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Unit",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(127)", maxLength: 127, nullable: false),
+                    Iri = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    TypeReferences = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Symbol = table.Column<string>(type: "nvarchar(31)", maxLength: 31, nullable: true),
+                    State = table.Column<string>(type: "nvarchar(31)", maxLength: 31, nullable: false),
+                    CompanyId = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(511)", maxLength: 511, nullable: true),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(127)", maxLength: 127, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Unit", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Node_Attribute",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NodeId = table.Column<int>(type: "int", nullable: false),
+                    AttributeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Node_Attribute", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Node_Attribute_Attribute_AttributeId",
+                        column: x => x.AttributeId,
+                        principalTable: "Attribute",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Node_Attribute_Node_NodeId",
+                        column: x => x.NodeId,
+                        principalTable: "Node",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Terminal_Attribute",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TerminalId = table.Column<int>(type: "int", nullable: false),
+                    AttributeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Terminal_Attribute", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Terminal_Attribute_Attribute_AttributeId",
+                        column: x => x.AttributeId,
+                        principalTable: "Attribute",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Terminal_Attribute_Terminal_TerminalId",
+                        column: x => x.TerminalId,
+                        principalTable: "Terminal",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Attribute_Unit",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AttributeId = table.Column<int>(type: "int", nullable: false),
+                    UnitId = table.Column<int>(type: "int", nullable: false),
+                    IsDefault = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Attribute_Unit", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Attribute_Unit_Attribute_AttributeId",
+                        column: x => x.AttributeId,
+                        principalTable: "Attribute",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Attribute_Unit_Unit_UnitId",
+                        column: x => x.UnitId,
+                        principalTable: "Unit",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Attribute_State",
+                table: "Attribute",
+                column: "State");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Attribute_Unit_AttributeId",
+                table: "Attribute_Unit",
+                column: "AttributeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Attribute_Unit_UnitId",
+                table: "Attribute_Unit",
+                column: "UnitId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Node_Attribute_AttributeId",
+                table: "Node_Attribute",
+                column: "AttributeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Node_Attribute_NodeId",
+                table: "Node_Attribute",
+                column: "NodeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Terminal_Attribute_AttributeId",
+                table: "Terminal_Attribute",
+                column: "AttributeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Terminal_Attribute_TerminalId",
+                table: "Terminal_Attribute",
+                column: "TerminalId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Unit_State",
+                table: "Unit",
+                column: "State");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Attribute_Unit");
+
+            migrationBuilder.DropTable(
+                name: "Node_Attribute");
+
+            migrationBuilder.DropTable(
+                name: "Terminal_Attribute");
+
+            migrationBuilder.DropTable(
+                name: "Unit");
+
+            migrationBuilder.DropTable(
+                name: "Attribute");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Attributes",
+                table: "Terminal",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Attributes",
+                table: "Node",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
--- src/server/TypeLibrary.Core/Migrations/TypeLibraryDbContextModelSnapshot.cs
+++ src/server/TypeLibrary.Core/Migrations/TypeLibraryDbContextModelSnapshot.cs
@@ -17,10 +17,66 @@ namespace TypeLibrary.Core.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "7.0.3")
+                .HasAnnotation("ProductVersion", "7.0.4")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
+
+            modelBuilder.Entity("TypeLibrary.Data.Models.AttributeLibDm", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("int")
+                        .HasColumnName("Id");
+
+                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
+
+                    b.Property<int>("CompanyId")
+                        .HasColumnType("int")
+                        .HasColumnName("CompanyId");
+
+                    b.Property<DateTime>("Created")
+                        .HasColumnType("datetime2")
+                        .HasColumnName("Created");
+
+                    b.Property<string>("CreatedBy")
+                        .IsRequired()
+                        .HasMaxLength(127)
+                        .HasColumnType("nvarchar(127)")
+                        .HasColumnName("CreatedBy");
+
+                    b.Property<string>("Description")
+                        .HasMaxLength(511)
+                        .HasColumnType("nvarchar(511)")
+                        .HasColumnName("Description");
+
+                    b.Property<string>("Iri")
+                        .HasMaxLength(255)
+                        .HasColumnType("nvarchar(255)")
+                        .HasColumnName("Iri");
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasMaxLength(127)
+                        .HasColumnType("nvarchar(127)")
+                        .HasColumnName("Name");
+
+                    b.Property<string>("State")
+                        .IsRequired()
+                        .HasMaxLength(31)
+                        .HasColumnType("nvarchar(31)")
+                        .HasColumnName("State");
+
+                    b.Property<string>("TypeReferences")
+                        .HasColumnType("nvarchar(max)")
+                        .HasColumnName("TypeReferences");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("State");
+
+                    b.ToTable("Attribute", (string)null);
+                });
 
             modelBuilder.Entity("TypeLibrary.Data.Models.AttributePredefinedLibDm", b =>
                 {
@@ -71,6 +127,34 @@ namespace TypeLibrary.Core.Migrations
                     b.HasKey("Key");
 
                     b.ToTable("AttributePredefined", (string)null);
+                });
+
+            modelBuilder.Entity("TypeLibrary.Data.Models.AttributeUnitLibDm", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("int")
+                        .HasColumnName("Id");
+
+                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
+
+                    b.Property<int>("AttributeId")
+                        .HasColumnType("int");
+
+                    b.Property<bool>("IsDefault")
+                        .HasColumnType("bit")
+                        .HasColumnName("IsDefault");
+
+                    b.Property<int>("UnitId")
+                        .HasColumnType("int");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("AttributeId");
+
+                    b.HasIndex("UnitId");
+
+                    b.ToTable("Attribute_Unit", (string)null);
                 });
 
             modelBuilder.Entity("TypeLibrary.Data.Models.LogLibDm", b =>
@@ -151,6 +235,30 @@ namespace TypeLibrary.Core.Migrations
                     b.ToTable("Log", (string)null);
                 });
 
+            modelBuilder.Entity("TypeLibrary.Data.Models.NodeAttributeLibDm", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("int")
+                        .HasColumnName("Id");
+
+                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
+
+                    b.Property<int>("AttributeId")
+                        .HasColumnType("int");
+
+                    b.Property<int>("NodeId")
+                        .HasColumnType("int");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("AttributeId");
+
+                    b.HasIndex("NodeId");
+
+                    b.ToTable("Node_Attribute", (string)null);
+                });
+
             modelBuilder.Entity("TypeLibrary.Data.Models.NodeLibDm", b =>
                 {
                     b.Property<int>("Id")
@@ -165,10 +273,6 @@ namespace TypeLibrary.Core.Migrations
                         .HasMaxLength(31)
                         .HasColumnType("nvarchar(31)")
                         .HasColumnName("Aspect");
-
-                    b.Property<string>("Attributes")
-                        .HasColumnType("nvarchar(max)")
-                        .HasColumnName("Attributes");
 
                     b.Property<int>("CompanyId")
                         .HasColumnType("int")
@@ -358,7 +462,7 @@ namespace TypeLibrary.Core.Migrations
                     b.ToTable("Symbol", (string)null);
                 });
 
-            modelBuilder.Entity("TypeLibrary.Data.Models.TerminalLibDm", b =>
+            modelBuilder.Entity("TypeLibrary.Data.Models.TerminalAttributeLibDm", b =>
                 {
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
@@ -367,9 +471,29 @@ namespace TypeLibrary.Core.Migrations
 
                     SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
 
-                    b.Property<string>("Attributes")
-                        .HasColumnType("nvarchar(max)")
-                        .HasColumnName("Attributes");
+                    b.Property<int>("AttributeId")
+                        .HasColumnType("int");
+
+                    b.Property<int>("TerminalId")
+                        .HasColumnType("int");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("AttributeId");
+
+                    b.HasIndex("TerminalId");
+
+                    b.ToTable("Terminal_Attribute", (string)null);
+                });
+
+            modelBuilder.Entity("TypeLibrary.Data.Models.TerminalLibDm", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("int")
+                        .HasColumnName("Id");
+
+                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
 
                     b.Property<string>("Color")
                         .HasColumnType("nvarchar(max)");
@@ -437,6 +561,105 @@ namespace TypeLibrary.Core.Migrations
                     b.ToTable("Terminal", (string)null);
                 });
 
+            modelBuilder.Entity("TypeLibrary.Data.Models.UnitLibDm", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("int")
+                        .HasColumnName("Id");
+
+                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
+
+                    b.Property<int>("CompanyId")
+                        .HasColumnType("int")
+                        .HasColumnName("CompanyId");
+
+                    b.Property<DateTime>("Created")
+                        .HasColumnType("datetime2")
+                        .HasColumnName("Created");
+
+                    b.Property<string>("CreatedBy")
+                        .IsRequired()
+                        .HasMaxLength(127)
+                        .HasColumnType("nvarchar(127)")
+                        .HasColumnName("CreatedBy");
+
+                    b.Property<string>("Description")
+                        .HasMaxLength(511)
+                        .HasColumnType("nvarchar(511)")
+                        .HasColumnName("Description");
+
+                    b.Property<string>("Iri")
+                        .HasMaxLength(255)
+                        .HasColumnType("nvarchar(255)")
+                        .HasColumnName("Iri");
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasMaxLength(127)
+                        .HasColumnType("nvarchar(127)")
+                        .HasColumnName("Name");
+
+                    b.Property<string>("State")
+                        .IsRequired()
+                        .HasMaxLength(31)
+                        .HasColumnType("nvarchar(31)")
+                        .HasColumnName("State");
+
+                    b.Property<string>("Symbol")
+                        .HasMaxLength(31)
+                        .HasColumnType("nvarchar(31)")
+                        .HasColumnName("Symbol");
+
+                    b.Property<string>("TypeReferences")
+                        .HasColumnType("nvarchar(max)")
+                        .HasColumnName("TypeReferences");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("State");
+
+                    b.ToTable("Unit", (string)null);
+                });
+
+            modelBuilder.Entity("TypeLibrary.Data.Models.AttributeUnitLibDm", b =>
+                {
+                    b.HasOne("TypeLibrary.Data.Models.AttributeLibDm", "Attribute")
+                        .WithMany("AttributeUnits")
+                        .HasForeignKey("AttributeId")
+                        .OnDelete(DeleteBehavior.NoAction)
+                        .IsRequired();
+
+                    b.HasOne("TypeLibrary.Data.Models.UnitLibDm", "Unit")
+                        .WithMany("UnitAttributes")
+                        .HasForeignKey("UnitId")
+                        .OnDelete(DeleteBehavior.NoAction)
+                        .IsRequired();
+
+                    b.Navigation("Attribute");
+
+                    b.Navigation("Unit");
+                });
+
+            modelBuilder.Entity("TypeLibrary.Data.Models.NodeAttributeLibDm", b =>
+                {
+                    b.HasOne("TypeLibrary.Data.Models.AttributeLibDm", "Attribute")
+                        .WithMany("AttributeNodes")
+                        .HasForeignKey("AttributeId")
+                        .OnDelete(DeleteBehavior.NoAction)
+                        .IsRequired();
+
+                    b.HasOne("TypeLibrary.Data.Models.NodeLibDm", "Node")
+                        .WithMany("NodeAttributes")
+                        .HasForeignKey("NodeId")
+                        .OnDelete(DeleteBehavior.NoAction)
+                        .IsRequired();
+
+                    b.Navigation("Attribute");
+
+                    b.Navigation("Node");
+                });
+
             modelBuilder.Entity("TypeLibrary.Data.Models.NodeLibDm", b =>
                 {
                     b.HasOne("TypeLibrary.Data.Models.NodeLibDm", "Parent")
@@ -466,6 +689,25 @@ namespace TypeLibrary.Core.Migrations
                     b.Navigation("Terminal");
                 });
 
+            modelBuilder.Entity("TypeLibrary.Data.Models.TerminalAttributeLibDm", b =>
+                {
+                    b.HasOne("TypeLibrary.Data.Models.AttributeLibDm", "Attribute")
+                        .WithMany("AttributeTerminals")
+                        .HasForeignKey("AttributeId")
+                        .OnDelete(DeleteBehavior.NoAction)
+                        .IsRequired();
+
+                    b.HasOne("TypeLibrary.Data.Models.TerminalLibDm", "Terminal")
+                        .WithMany("TerminalAttributes")
+                        .HasForeignKey("TerminalId")
+                        .OnDelete(DeleteBehavior.NoAction)
+                        .IsRequired();
+
+                    b.Navigation("Attribute");
+
+                    b.Navigation("Terminal");
+                });
+
             modelBuilder.Entity("TypeLibrary.Data.Models.TerminalLibDm", b =>
                 {
                     b.HasOne("TypeLibrary.Data.Models.TerminalLibDm", "Parent")
@@ -476,9 +718,20 @@ namespace TypeLibrary.Core.Migrations
                     b.Navigation("Parent");
                 });
 
+            modelBuilder.Entity("TypeLibrary.Data.Models.AttributeLibDm", b =>
+                {
+                    b.Navigation("AttributeNodes");
+
+                    b.Navigation("AttributeTerminals");
+
+                    b.Navigation("AttributeUnits");
+                });
+
             modelBuilder.Entity("TypeLibrary.Data.Models.NodeLibDm", b =>
                 {
                     b.Navigation("Children");
+
+                    b.Navigation("NodeAttributes");
 
                     b.Navigation("NodeTerminals");
                 });
@@ -487,7 +740,14 @@ namespace TypeLibrary.Core.Migrations
                 {
                     b.Navigation("Children");
 
+                    b.Navigation("TerminalAttributes");
+
                     b.Navigation("TerminalNodes");
+                });
+
+            modelBuilder.Entity("TypeLibrary.Data.Models.UnitLibDm", b =>
+                {
+                    b.Navigation("UnitAttributes");
                 });
 #pragma warning restore 612, 618
         }
