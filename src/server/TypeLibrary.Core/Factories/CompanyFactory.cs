05b3a4c8019434053ff3d66ffb3fc690522829eb
Jørgen Drejer Norheim <jorgen.norheim@bouvet.no>
2023-04-04 08:56:55 +0200

    Added exception handling to company factory

--- src/server/TypeLibrary.Core/Factories/CompanyFactory.cs
+++ src/server/TypeLibrary.Core/Factories/CompanyFactory.cs
@@ -1,7 +1,6 @@
-using System.Collections.Generic;
-using System.Linq;
+using Microsoft.Extensions.Logging;
 using Mimirorg.Authentication.Contracts;
-using Mimirorg.TypeLibrary.Models.Client;
+using Mimirorg.Common.Exceptions;
 using TypeLibrary.Data.Contracts.Common;
 
 namespace TypeLibrary.Core.Factories;
@@ -9,11 +8,13 @@ namespace TypeLibrary.Core.Factories;
 public class CompanyFactory : ICompanyFactory
 {
     private readonly ICacheRepository _companyCache;
+    private readonly ILogger<CompanyFactory> _logger;
     private readonly IMimirorgCompanyService _mimirorgCompanyService;
 
-    public CompanyFactory(ICacheRepository companyCache, IMimirorgCompanyService mimirorgCompanyService)
+    public CompanyFactory(ICacheRepository companyCache, ILogger<CompanyFactory> logger, IMimirorgCompanyService mimirorgCompanyService)
     {
         _companyCache = companyCache;
+        _logger = logger;
         _mimirorgCompanyService = mimirorgCompanyService;
     }
 
@@ -21,7 +22,17 @@ public class CompanyFactory : ICompanyFactory
     {
         if (companyId == null) return null;
 
-        var companyName = _companyCache.GetOrCreateAsync($"company-{companyId}", async () => await _mimirorgCompanyService.GetCompanyById(companyId.Value)).Result.Name;
+        string companyName = null;
+
+        try
+        {
+            companyName = _companyCache.GetOrCreateAsync($"company-{companyId}",
+                async () => await _mimirorgCompanyService.GetCompanyById(companyId.Value)).Result.Name;
+        }
+        catch (MimirorgNotFoundException exception)
+        {
+            _logger.LogError($"Error when getting company name: {exception.Message}");
+        }
 
         return companyName;
     }
