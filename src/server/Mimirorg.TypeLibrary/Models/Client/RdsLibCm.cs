bf00239348c9e4f2be43bfbd160a8d49d7e6f30d
Jørgen Drejer Norheim <jorgen.norheim@bouvet.no>
2023-04-17 12:50:44 +0200

    Added new models and configs

--- src/server/Mimirorg.TypeLibrary/Models/Client/RdsLibCm.cs
+++ src/server/Mimirorg.TypeLibrary/Models/Client/RdsLibCm.cs
@@ -1,9 +1,21 @@
+using Mimirorg.Common.Enums;
+
 namespace Mimirorg.TypeLibrary.Models.Client;
 
 public class RdsLibCm
 {
     public string Id { get; set; }
+    public string RdsCode { get; set; }
     public string Name { get; set; }
     public string Iri { get; set; }
+    public string TypeReference { get; set; }
+    public DateTime Created { get; set; }
+    public string CreatedBy { get; set; }
+    public int? CompanyId { get; set; }
+    public string CompanyName { get; set; }
+    public State State { get; set; }
+    public string Description { get; set; }
+    public string CategoryId { get; set; }
+    public string CategoryName { get; set; }
     public string Kind => nameof(RdsLibCm);
 }
--- src/server/TypeLibrary.Data/Models/RdsLibDm.cs
+++ src/server/TypeLibrary.Data/Models/RdsLibDm.cs
@@ -1,8 +1,39 @@
+using System;
+using Mimirorg.Common.Contracts;
+using Mimirorg.Common.Enums;
+using Mimirorg.TypeLibrary.Enums;
+using Mimirorg.TypeLibrary.Models.Application;
+using TypeLibrary.Data.Contracts.Common;
+
 namespace TypeLibrary.Data.Models;
 
-public class RdsLibDm
+public class RdsLibDm : ILogable, IStatefulObject
 {
     public string Id { get; set; }
+    public string RdsCode { get; set; }
     public string Name { get; set; }
     public string Iri { get; set; }
+    public string TypeReference { get; set; }
+    public DateTime Created { get; set; }
+    public string CreatedBy { get; set; }
+    public int? CompanyId { get; set; }
+    public State State { get; set; }
+    public string Description { get; set; }
+    public string CategoryId { get; set; }
+    public CategoryLibDm Category { get; set; }
+
+    public LogLibAm CreateLog(LogType logType, string logTypeValue, string comment)
+    {
+        return new LogLibAm
+        {
+            ObjectId = Id,
+            ObjectFirstVersionId = Id,
+            ObjectType = nameof(RdsLibDm),
+            ObjectName = Name,
+            ObjectVersion = "",
+            LogType = logType,
+            LogTypeValue = logTypeValue,
+            Comment = comment
+        };
+    }
 }
