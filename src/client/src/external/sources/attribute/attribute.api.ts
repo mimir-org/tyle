39d6ac591dc2ffb289425de5901ee79d24284a8c
Mathias Bøe <mathias.boe@bouvet.no>
2023-04-17 14:39:30 +0200

    Adding dummy queries for attributes, these need to be fixed at a later point to work correctly.

--- src/client/src/external/sources/attribute/attribute.api.ts
+++ src/client/src/external/sources/attribute/attribute.api.ts
@@ -3,6 +3,7 @@ import {
   AttributePredefinedLibCm,
   QuantityDatumLibCm,
   QuantityDatumType,
+  AttributeLibAm,
 } from "@mimirorg/typelibrary-types";
 import { apiClient } from "external/client/apiClient";
 
@@ -12,10 +13,16 @@ export const attributeApi = {
   getAttributes() {
     return apiClient.get<AttributeLibCm[]>(_basePath).then((r) => r.data);
   },
+  getAttribute(id?: string) {
+    return apiClient.get<AttributeLibCm>(`${_basePath}/${id}`).then((r) => r.data);
+  },
   getAttributesPredefined() {
     return apiClient.get<AttributePredefinedLibCm[]>(`${_basePath}/predefined`).then((r) => r.data);
   },
   getQuantityDatum(datumType: QuantityDatumType) {
     return apiClient.get<QuantityDatumLibCm[]>(`${_basePath}/datum/${datumType}`).then((r) => r.data);
+  },
+  putAttribute(item: AttributeLibAm, id?: string) {
+    return apiClient.put<AttributeLibCm>(`${_basePath}/${id}`, item).then((r) => r.data);
   },
 };
--- src/client/src/external/sources/attribute/attribute.queries.ts
+++ src/client/src/external/sources/attribute/attribute.queries.ts
@@ -1,6 +1,6 @@
-import { QuantityDatumType } from "@mimirorg/typelibrary-types";
-import { useQuery } from "@tanstack/react-query";
-import { attributeApi } from "external/sources/attribute/attribute.api";
+import { QuantityDatumType, AttributeLibAm } from "@mimirorg/typelibrary-types";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { attributeApi } from "./attribute.api";
 
 const keys = {
   allAttributes: ["attributes"] as const,
@@ -9,11 +9,23 @@ const keys = {
   predefinedLists: () => [...keys.allPredefined, "list"] as const,
   allQuantityDatum: ["quantityDatum"] as const,
   quantityDatum: (datumType: QuantityDatumType) => [...keys.allQuantityDatum, datumType] as const,
+  attribute: (id?: string) => [...keys.attributeLists(), id] as const,
 };
 
 export const useGetAttributes = () => useQuery(keys.attributeLists(), attributeApi.getAttributes);
 
 export const useGetAttributesPredefined = () => useQuery(keys.predefinedLists(), attributeApi.getAttributesPredefined);
 
+export const useGetAttribute = (id?: string) =>
+  useQuery(keys.attribute(id), () => attributeApi.getAttribute(id), { enabled: !!id, retry: false });
+
 export const useGetQuantityDatum = (datumType: QuantityDatumType) =>
   useQuery(keys.quantityDatum(datumType), () => attributeApi.getQuantityDatum(datumType));
+
+export const useUpdateAttributes = (id?: string) => {
+  const queryClient = useQueryClient();
+
+  return useMutation((item: AttributeLibAm) => attributeApi.putAttribute(item, id), {
+    onSuccess: (unit) => queryClient.invalidateQueries(keys.attribute(unit.id)),
+  });
+};
