de640ce03b2ae93037b492e8a36c76f370aa912e
Jørgen Drejer Norheim <jorgen.norheim@bouvet.no>
2023-04-17 13:08:04 +0200

    Added profile for rds mappings

--- src/server/TypeLibrary.Core/Profiles/RdsProfile.cs
+++ src/server/TypeLibrary.Core/Profiles/RdsProfile.cs
@@ -1,16 +1,49 @@
+using System;
 using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Mimirorg.Common.Extensions;
+using Mimirorg.TypeLibrary.Models.Application;
 using Mimirorg.TypeLibrary.Models.Client;
+using TypeLibrary.Core.Factories;
+using TypeLibrary.Core.Profiles.Resolvers;
+using TypeLibrary.Data.Contracts;
 using TypeLibrary.Data.Models;
 
 namespace TypeLibrary.Core.Profiles;
 
 public class RdsProfile : Profile
 {
-    public RdsProfile()
+    public RdsProfile(IApplicationSettingsRepository settings, IHttpContextAccessor contextAccessor, ICompanyFactory companyFactory)
     {
+        CreateMap<RdsLibAm, RdsLibDm>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => Guid.NewGuid().ToString()))
+            .ForMember(dest => dest.RdsCode, opt => opt.MapFrom(src => src.RdsCode))
+            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+            .ForMember(dest => dest.Iri, opt => opt.MapFrom(new RdsIriResolver(settings)))
+            .ForMember(dest => dest.TypeReference, opt => opt.MapFrom(src => src.TypeReference))
+            .ForMember(dest => dest.Created, opt => opt.MapFrom(src => DateTime.UtcNow))
+            .ForMember(dest => dest.CreatedBy,
+                opt => opt.MapFrom(src =>
+                    string.IsNullOrWhiteSpace(contextAccessor.GetUserId()) ? "Unknown" : contextAccessor.GetUserId()))
+            .ForMember(dest => dest.CompanyId, opt => opt.MapFrom(src => src.CompanyId))
+            .ForMember(dest => dest.State, opt => opt.Ignore())
+            .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description))
+            .ForMember(dest => dest.CategoryId, opt => opt.MapFrom(src => src.CategoryId))
+            .ForMember(dest => dest.Category, opt => opt.Ignore());
+
         CreateMap<RdsLibDm, RdsLibCm>()
             .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+            .ForMember(dest => dest.RdsCode, opt => opt.MapFrom(src => src.RdsCode))
             .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
-            .ForMember(dest => dest.Iri, opt => opt.MapFrom(src => src.Iri));
+            .ForMember(dest => dest.Iri, opt => opt.MapFrom(src => src.Iri))
+            .ForMember(dest => dest.TypeReference, opt => opt.MapFrom(src => src.TypeReference))
+            .ForMember(dest => dest.Created, opt => opt.MapFrom(src => src.Created))
+            .ForMember(dest => dest.CreatedBy, opt => opt.MapFrom(src => src.CreatedBy))
+            .ForMember(dest => dest.CompanyId, opt => opt.MapFrom(src => src.CompanyId))
+            .ForMember(dest => dest.CompanyName, opt => opt.MapFrom(src => companyFactory.GetCompanyName(src.CompanyId)))
+            .ForMember(dest => dest.State, opt => opt.MapFrom(src => src.State))
+            .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description))
+            .ForMember(dest => dest.CategoryId, opt => opt.MapFrom(src => src.CategoryId))
+            .ForMember(dest => dest.CategoryName, opt => opt.MapFrom(src => src.Category.Name));
     }
 }
