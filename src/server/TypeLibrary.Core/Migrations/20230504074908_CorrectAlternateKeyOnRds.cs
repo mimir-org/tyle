93e90eff1e5cdb79621fa20ec1387846a4157630
Jørgen Drejer Norheim <jorgen.norheim@bouvet.no>
2023-05-04 09:59:50 +0200

    Fixed an error in the alternate key for the rds table

---
+++ src/server/TypeLibrary.Core/Migrations/20230504074908_CorrectAlternateKeyOnRds.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TypeLibrary.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class CorrectAlternateKeyOnRds : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropUniqueConstraint(
+                name: "AK_Rds_RdsCode_Name",
+                table: "Rds");
+
+            migrationBuilder.AddUniqueConstraint(
+                name: "AK_Rds_RdsCode_CategoryId",
+                table: "Rds",
+                columns: new[] { "RdsCode", "CategoryId" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropUniqueConstraint(
+                name: "AK_Rds_RdsCode_CategoryId",
+                table: "Rds");
+
+            migrationBuilder.AddUniqueConstraint(
+                name: "AK_Rds_RdsCode_Name",
+                table: "Rds",
+                columns: new[] { "RdsCode", "Name" });
+        }
+    }
+}
