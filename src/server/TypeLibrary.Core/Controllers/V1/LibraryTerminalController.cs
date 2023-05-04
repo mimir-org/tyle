10859826b8dc721505788c27c0a32707c6f0600f
Jørgen Drejer Norheim <jorgen.norheim@bouvet.no>
2023-05-04 11:07:46 +0200

    Added some more missing return codes

--- src/server/TypeLibrary.Core/Controllers/V1/LibraryTerminalController.cs
+++ src/server/TypeLibrary.Core/Controllers/V1/LibraryTerminalController.cs
@@ -177,6 +177,7 @@ public class LibraryTerminalController : ControllerBase
     [ProducesResponseType(typeof(TerminalLibCm), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     [Authorize]
     public async Task<IActionResult> ChangeState([FromRoute] string id, [FromRoute] State state)
--- src/server/TypeLibrary.Core/Controllers/V1/LibraryUnitController.cs
+++ src/server/TypeLibrary.Core/Controllers/V1/LibraryUnitController.cs
@@ -175,6 +175,7 @@ public class LibraryUnitController : ControllerBase
     [ProducesResponseType(typeof(ApprovalDataCm), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     [Authorize]
     public async Task<IActionResult> ChangeState([FromRoute] string id, [FromRoute] State state)
