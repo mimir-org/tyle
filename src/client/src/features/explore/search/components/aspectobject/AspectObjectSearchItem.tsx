f698d0e2530f96c85c83e35b7f0a291d171d389e
Mathias Bøe <mathias.boe@bouvet.no>
2023-05-08 12:06:32 +0200

    Removing dead code from AspectObjectSearchItem.tsx

--- src/client/src/features/explore/search/components/aspectobject/AspectObjectSearchItem.tsx
+++ src/client/src/features/explore/search/components/aspectobject/AspectObjectSearchItem.tsx
@@ -1,17 +1,9 @@
-import { State } from "@mimirorg/typelibrary-types";
-import { DocumentDuplicate, PencilSquare, Trash, ChevronUp } from "@styled-icons/heroicons-outline";
 import { AspectObjectItem } from "common/types/aspectObjectItem";
 import { UserItem } from "common/types/userItem";
-import { Button } from "complib/buttons";
-import { AlertDialog } from "complib/overlays";
-import { usePatchAspectObjectState } from "external/sources/aspectobject/aspectObject.queries";
 import { AspectObjectPreview } from "features/common/aspectobject";
-import { PlainLink } from "features/common/plain-link";
 import { Item } from "features/explore/search/components/item/Item";
 import { ItemDescription } from "features/explore/search/components/item/ItemDescription";
-import { useTranslation } from "react-i18next";
-import { useTheme } from "styled-components";
-import { useButtonStateFilter } from "features/explore/search/hooks/useButtonFilter";
+import { SearchItemActions } from "../SearchItemActions";
 
 export type AspectObjectSearchItemProps = AspectObjectItem & {
   isSelected?: boolean;
@@ -38,109 +30,6 @@ export const AspectObjectSearchItem = ({
     isSelected={isSelected}
     preview={<AspectObjectPreview {...aspectObject} />}
     description={<ItemDescription onClick={setSelected} {...aspectObject} />}
-    actions={<AspectObjectSearchItemActions user={user} aspectObject={aspectObject} />}
+    actions={<SearchItemActions user={user} item={aspectObject} />}
   />
 );
-
-type AspectObjectSearchItemActionProps = {
-  user?: UserItem;
-  aspectObject?: AspectObjectItem;
-};
-
-const AspectObjectSearchItemActions = ({ user, aspectObject }: AspectObjectSearchItemActionProps) => {
-  const theme = useTheme();
-  const { t } = useTranslation("explore");
-  const patcMutation = usePatchAspectObjectState();
-  const btnFilter = useButtonStateFilter(aspectObject ?? null, user);
-
-  if (user == null || aspectObject == null) return <></>;
-
-  const deleteAction = {
-    name: t("search.item.delete"),
-    onAction: () => patcMutation.mutate({ id: aspectObject.id, state: State.Delete }),
-  };
-
-  const approveAction = {
-    name: t("search.item.approve"),
-    onAction: () => patcMutation.mutate({ id: aspectObject.id, state: State.Approve }),
-  };
-
-  const cloneLink = btnFilter.clone ? `/form/aspectObject/clone/${aspectObject.id}` : "#";
-  const editLink = btnFilter.edit ? `/form/aspectObject/edit/${aspectObject.id}` : "#";
-
-  return (
-    <>
-      <PlainLink tabIndex={-1} to={cloneLink}>
-        <Button
-          disabled={!btnFilter.clone}
-          tabIndex={0}
-          as={!btnFilter.clone ? "button" : "span"}
-          icon={<DocumentDuplicate />}
-          iconOnly
-        >
-          {t("search.item.clone")}
-        </Button>
-      </PlainLink>
-      <PlainLink tabIndex={-1} to={editLink}>
-        <Button
-          disabled={!btnFilter.edit}
-          tabIndex={0}
-          as={!btnFilter.edit ? "button" : "span"}
-          icon={<PencilSquare />}
-          iconOnly
-        >
-          {t("search.item.edit")}
-        </Button>
-      </PlainLink>
-      <AlertDialog
-        gap={theme.tyle.spacing.multiple(6)}
-        actions={[deleteAction]}
-        title={t("search.item.templates.delete")}
-        description={t("search.item.deleteDescription")}
-        hideDescription
-        content={
-          <AspectObjectPreview
-            name={aspectObject.name}
-            color={aspectObject.color}
-            img={aspectObject.img}
-            terminals={aspectObject.terminals}
-          />
-        }
-      >
-        <Button
-          disabled={!btnFilter.delete}
-          variant={btnFilter.deleted ? "outlined" : "filled"}
-          icon={<Trash />}
-          iconOnly
-        >
-          {t("search.item.delete")}
-        </Button>
-      </AlertDialog>
-
-      <AlertDialog
-        gap={theme.tyle.spacing.multiple(6)}
-        actions={[approveAction]}
-        title={t("search.item.templates.approve")}
-        description={t("search.item.approveDescription")}
-        hideDescription
-        content={
-          <AspectObjectPreview
-            name={aspectObject.name}
-            color={aspectObject.color}
-            img={aspectObject.img}
-            terminals={aspectObject.terminals}
-          />
-        }
-      >
-        <Button
-          disabled={!btnFilter.approve}
-          variant={btnFilter.approved ? "outlined" : "filled"}
-          icon={<ChevronUp />}
-          iconOnly
-        >
-          {t("search.item.approve")}
-        </Button>
-      </AlertDialog>
-    </>
-  );
-};
