c9d8d448efd09cd94fd2d806c0ee6705439058de
Mathias Bøe <mathias.boe@bouvet.no>
2023-05-15 14:47:34 +0200

    Added mvp icons for entities

---
+++ src/client/src/features/icons/AttributeIcon.tsx
@@ -0,0 +1,40 @@
+const AttributeIcon = () => (
+  <svg
+    width="100%"
+    height="100%"
+    viewBox="0 0 61 42"
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    xmlSpace="preserve"
+    style={{
+      fillRule: "evenodd",
+      clipRule: "evenodd",
+      strokeLinecap: "round",
+      strokeLinejoin: "round",
+      strokeMiterlimit: "1.5",
+    }}
+  >
+    <g transform="matrix(50,0,0,50,51.0417,36.3444)"></g>
+    <text
+      x="26.042px"
+      y="36.344px"
+      style={{
+        fontFamily: "ArialMT, Arial, sans-serif",
+        fontSize: "50px",
+      }}
+    >
+      x
+    </text>
+    <path
+      d="M2.083,18.75l8.334,0l4.166,20.833l4.167,-37.5l39.583,0"
+      style={{
+        fill: "none",
+        stroke: "#000",
+        strokeWidth: "4.17px",
+      }}
+    />
+  </svg>
+);
+
+export default AttributeIcon;
