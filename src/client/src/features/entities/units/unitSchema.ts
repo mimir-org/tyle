4a958464ea64da530fef3ddfadf8c1ad82a43ba4
Jørgen Drejer Norheim <jorgen.norheim@bouvet.no>
2023-05-24 13:36:05 +0200

    Added unit schema

--- src/client/src/features/entities/units/unitSchema.ts
+++ src/client/src/features/entities/units/unitSchema.ts
@@ -1,8 +1,12 @@
-import { object, string } from "yup";
+import { UnitLibAm } from "@mimirorg/typelibrary-types";
+import { YupShape } from "common/types/yupShape";
+import { TFunction } from "react-i18next";
+import * as yup from "yup";
 
-export const unitSchema = object({
-  name: string().max(60).required(),
-  typeReference: string().required(),
-  symbol: string().required(),
-  description: string().max(500).required(),
+export const unitSchema = (t: TFunction<"translation">) => 
+  yup.object<YupShape<UnitLibAm>>({
+  name: yup.string().max(120, t("unit.validation.name.max")).required(),
+  description: yup.string().max(500).required(),
+  typeReference: yup.string(),
+  symbol: yup.string().max(30, t("unit.validation.symbol.max")).required(),
 });
