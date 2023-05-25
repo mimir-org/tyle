155fb6e4bbf2e0e5feaac2d4537af022d34303b0
Jørgen Drejer Norheim <jorgen.norheim@bouvet.no>
2023-05-25 11:51:13 +0200

    Created variable for formunithelper shape

--- src/client/src/features/entities/attributes/attributeSchema.ts
+++ src/client/src/features/entities/attributes/attributeSchema.ts
@@ -3,23 +3,20 @@ import { TFunction } from "react-i18next";
 import * as yup from "yup";
 import { FormAttributeLib } from "./types/formAttributeLib";
 
+const formUnitHelperShape = {
+  name: yup.string().max(120).required(),
+  description: yup.string().max(500),
+  symbol: yup.string().max(30),
+  unitId: yup.string().required(),
+};
+
 export const attributeSchema = (t: TFunction<"translation">) =>
   yup.object<YupShape<FormAttributeLib>>({
     name: yup.string().max(120, t("common.validation.name.max")).required(t("common.validation.name.required")),
     typeReference: yup.string().max(255),
     description: yup.string().max(500, t("common.validation.description.max")),
     units: yup.array().of(
-      yup.object({
-        name: yup.string().max(120).required(),
-        description: yup.string().max(500),
-        symbol: yup.string().max(30),
-        unitId: yup.string().required(),
-      })
+      yup.object(formUnitHelperShape)
     ).nullable(),
-    defaultUnit: yup.object({
-        name: yup.string().max(120).required(),
-        description: yup.string().max(500),
-        symbol: yup.string().max(30),
-        unitId: yup.string().required(),
-      }).nullable(),
+    defaultUnit: yup.object(formUnitHelperShape).nullable(),
   });
