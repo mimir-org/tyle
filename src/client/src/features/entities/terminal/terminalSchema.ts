f018c2fcd5072daea1bbcd72d619b575fe6ad9cb
Jørgen Drejer Norheim <jorgen.norheim@bouvet.no>
2023-05-31 15:29:21 +0200

    Fixed wrong value in terminal schema

--- src/client/src/features/entities/terminal/terminalSchema.ts
+++ src/client/src/features/entities/terminal/terminalSchema.ts
@@ -5,7 +5,7 @@ import * as yup from "yup";
 
 export const terminalSchema = (t: TFunction<"translation">) =>
   yup.object<YupShape<FormTerminalLib>>({
-    name: yup.string().max(60, t("common.validation.name.max")).required(t("common.validation.name.required")),
+    name: yup.string().max(120, t("common.validation.name.max")).required(t("common.validation.name.required")),
     typeReference: yup.string().max(255),
     color: yup.string().max(7).required(t("terminal.validation.color.required")),
     description: yup.string().max(500, t("common.validation.description.max")),
