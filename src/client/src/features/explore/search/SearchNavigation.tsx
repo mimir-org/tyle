5e9ef903adbf6c41beb598e6298bc2c7a907dd74
Mathias Bøe <mathias.boe@bouvet.no>
2023-06-01 12:09:49 +0200

    Fixing up some of the pagination

--- src/client/src/features/explore/search/SearchNavigation.tsx
+++ src/client/src/features/explore/search/SearchNavigation.tsx
@@ -1,63 +1,85 @@
 import { ChevronDoubleLeft, ChevronDoubleRight, ChevronLeft, ChevronRight } from "@styled-icons/heroicons-outline";
 import { Button } from "complib/buttons";
-import { Box } from "complib/layouts";
+import { Flexbox } from "complib/layouts";
 import { useSearchParams } from "react-router-dom";
+import { useTheme } from "styled-components";
 
 interface SearchNavigationProps {
   numPages: number;
 }
 
+/**
+ * Returns the start and end of the pagination range.
+ * @param pageNum - the current page number
+ * @param numPages - the total number of pages
+ * @param range - the range of pages to show on either side of the current page
+ */
+const getPaginationRange = (pageNum: number, numPages: number, range: number) => {
+  const start = Math.max(1, pageNum - range);
+  const end = Math.min(numPages, pageNum + range);
+  return { start, end };
+};
+
 export const SearchNavigation = ({ numPages }: SearchNavigationProps) => {
+  const theme = useTheme();
   const [searchParams, setSearchParams] = useSearchParams();
   const pageNum = Number(searchParams.get("page"));
+  const { start, end } = getPaginationRange(pageNum, numPages, 2);
 
   return (
-    <Box>
-      {pageNum > 1 && (
-        <>
-          <Button variant="filled" icon={<ChevronDoubleLeft />} iconOnly onClick={() => setSearchParams({ page: "1" })}>
-            First
-          </Button>
-          <Button
-            variant="filled"
-            icon={<ChevronLeft />}
-            iconOnly
-            onClick={() => setSearchParams({ page: String(pageNum - 1) })}
-          >
-            Previous
-          </Button>
-        </>
-      )}
-      {[...Array(numPages).keys()].map((x) => (
+    <Flexbox gap={theme.tyle.spacing.s} alignItems={"center"} justifyContent={"center"}>
+      <Flexbox justifyContent={"center"}>
         <Button
-          key={"navButton" + x}
           variant="filled"
-          onClick={() => setSearchParams({ page: String(x + 1) })}
-          disabled={x + 1 === pageNum}
+          icon={<ChevronDoubleLeft />}
+          iconOnly
+          onClick={() => setSearchParams({ page: "1" })}
+          disabled={pageNum <= 1}
         >
-          {x + 1}
+          First
         </Button>
-      ))}
-      {pageNum < numPages && (
-        <>
+        <Button
+          variant="filled"
+          icon={<ChevronLeft />}
+          iconOnly
+          onClick={() => setSearchParams({ page: String(pageNum - 1) })}
+          disabled={pageNum === 1}
+        >
+          Previous
+        </Button>
+      </Flexbox>
+      {[...Array(numPages).keys()]
+        .filter((x) => x + 1 >= start && x + 1 <= end)
+        .map((x) => (
           <Button
+            key={"navButton" + x}
             variant="filled"
-            icon={<ChevronRight />}
-            iconOnly
-            onClick={() => setSearchParams({ page: String(pageNum + 1) })}
+            onClick={() => setSearchParams({ page: String(x + 1) })}
+            disabled={x + 1 === pageNum}
           >
-            Next
+            {x + 1}
           </Button>
-          <Button
-            variant="filled"
-            icon={<ChevronDoubleRight />}
-            iconOnly
-            onClick={() => setSearchParams({ page: String(numPages) })}
-          >
-            Last
-          </Button>
-        </>
-      )}
-    </Box>
+        ))}
+      <Flexbox>
+        <Button
+          variant="filled"
+          icon={<ChevronRight />}
+          iconOnly
+          onClick={() => setSearchParams({ page: String(pageNum + 1) })}
+          disabled={pageNum >= numPages}
+        >
+          Next
+        </Button>
+        <Button
+          variant="filled"
+          icon={<ChevronDoubleRight />}
+          iconOnly
+          onClick={() => setSearchParams({ page: String(numPages) })}
+          disabled={pageNum >= numPages}
+        >
+          Last
+        </Button>
+      </Flexbox>
+    </Flexbox>
   );
 };
