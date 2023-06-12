cd4e92cf33357edf8b28dff9554fb6cfb256bff4
Jørgen Drejer Norheim <jorgen.norheim@bouvet.no>
2023-06-12 20:36:56 +0200

    test commit

--- src/client/src/features/ui/App.tsx
+++ src/client/src/features/ui/App.tsx
@@ -9,6 +9,8 @@ export const App = () => {
   const { data: user, isSuccess, isLoading } = useGetCurrentUser();
   const isLoggedIn = isSuccess && user;
 
+  console.log(isLoggedIn);
+
   if (isLoading) {
     return <FullPageSpinner text={t("global.loading")} />;
   }
