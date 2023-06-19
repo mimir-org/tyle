465cb3f1a6c38c7a384cf62af39877cd7da9ea74
Jørgen Drejer Norheim <jorgen.norheim@bouvet.no>
2023-06-19 14:35:55 +0200

    Add method to get logs for specific object

--- src/server/TypeLibrary.Data/Contracts/ILogRepository.cs
+++ src/server/TypeLibrary.Data/Contracts/ILogRepository.cs
@@ -13,6 +13,13 @@ public interface ILogRepository
     IEnumerable<LogLibDm> Get();
 
     /// <summary>
+    /// Get all logs for a specific object
+    /// </summary>
+    /// <param name="objectId">The id of the object to get logs for</param>
+    /// <returns></returns>
+    IEnumerable<LogLibDm> Get(string objectId);
+
+    /// <summary>
     /// Create a log entry
     /// </summary>
     /// <param name="logDms">The log entry to be created</param>
