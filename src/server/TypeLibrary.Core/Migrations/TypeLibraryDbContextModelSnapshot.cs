05d640d0236fa3f4960c736efc188334fb03116c
Jørgen Drejer Norheim <jorgen.norheim@bouvet.no>
2023-06-19 14:49:32 +0200

    Migration

--- src/server/TypeLibrary.Core/Migrations/TypeLibraryDbContextModelSnapshot.cs
+++ src/server/TypeLibrary.Core/Migrations/TypeLibraryDbContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace TypeLibrary.Core.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "7.0.5")
+                .HasAnnotation("ProductVersion", "7.0.7")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
