c69da8e82d45e5a150a2179338c2e9a4396446ad
Jørgen Drejer Norheim <jorgen.norheim@bouvet.no>
2023-06-30 14:50:51 +0200

    Deleted unused files

--- src/server/Mimirorg.Common/Extensions/TaskExtensions.cs
+++
@@ -1,9 +0,0 @@
-namespace Mimirorg.Common.Extensions;
-
-public static class TaskExtensions
-{
-    public static async Task<(T1, T2)> WhenAll<T1, T2>(Task<T1> task1, Task<T2> task2)
-    {
-        return (await task1, await task2);
-    }
-}
