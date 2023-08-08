71068bcc35439c4d90b14094c578005e91f271b5
Jørgen Drejer Norheim <jorgen.norheim@bouvet.no>
2023-08-08 14:21:44 +0200

    Installed vite and did config

--- src/client/src/common/utils/config.ts
+++ src/client/src/common/utils/config.ts
@@ -1,10 +1,10 @@
-export const isProduction = process.env.NODE_ENV !== "development";
+export const isProduction = import.meta.env.NODE_ENV !== "development";
 
 const assignValue = (key: keyof TypeLibraryEnv) =>
-  isProduction ? window.__TYPELIBRARY_ENV[key] : process.env[`REACT_APP_${key}`];
+  isProduction ? window.__TYPELIBRARY_ENV[key] : import.meta.env[`VITE_${key}`];
 
 const assignVersion = (key: keyof TypeLibraryEnv) =>
-  isProduction ? window.__TYPELIBRARY_ENV[key] : process.env[`REACT_APP_${key}`];
+  isProduction ? window.__TYPELIBRARY_ENV[key] : import.meta.env[`VITE_${key}`];
 
 const config: TypeLibraryEnv = {
   API_BASE_URL: assignValue("API_BASE_URL"),
---
+++ src/client/vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+import svgrPlugin from 'vite-plugin-svgr';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // This changes the out put dir from dist to build
+  // comment this out if that isn't relevant for your project
+  build: {
+    outDir: 'build',
+  },
+  plugins: [
+    react(),
+    viteTsconfigPaths(),
+    svgrPlugin({
+      svgrOptions: {
+        icon: true,
+        // ...svgr options (https://react-svgr.com/docs/options/)
+      },
+    }),
+  ],
+});
