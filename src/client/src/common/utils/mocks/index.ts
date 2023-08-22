654bb9f87311cc3f853ffcd33121962b33a1712b
Jørgen Drejer Norheim <122889083+jorgen-norheim@users.noreply.github.com>
2023-08-22 15:08:21 +0200

    Removed unused packages and updated (#742)
    * Removed storybook
    
    * Removed stories
    
    * Removed mocks
    
    * Updated packages

--- src/client/src/common/utils/mocks/index.ts
+++
@@ -1,3 +0,0 @@
-export { mockAspectObjectItem } from "common/utils/mocks/mockAspectObjectItem";
-export { mockAspectObjectTerminalItem } from "common/utils/mocks/mockAspectObjectTerminalItem";
-export { mockInfoItem } from "common/utils/mocks/mockInfoItem";
