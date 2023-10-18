ca95a04724de73598e8c2ec9acf3ec1eb4c36823
Jørgen Drejer Norheim <jorgen.norheim@bouvet.no>
2023-10-18 14:03:02 +0200

    Refactor IMF namespace IRIs, add IRIs

---
+++ src/server/Tyle.Converters/Iris/Skos.cs
@@ -0,0 +1,8 @@
+namespace Tyle.Converters.Iris;
+
+public class Skos
+{
+    private const string NameSpace = "http://www.w3.org/2004/02/skos/core#";
+
+    public static readonly Uri Notation = new($"{NameSpace}notation");
+}
