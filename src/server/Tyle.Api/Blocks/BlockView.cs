1b3bb343cc1ba92d166583aca0b5055c2f606f5e
Jørgen Drejer Norheim <jorgen.norheim@bouvet.no>
2023-11-08 12:26:39 +0100

    Update block view to use symbol, not string

--- src/server/Tyle.Api/Blocks/BlockView.cs
+++ src/server/Tyle.Api/Blocks/BlockView.cs
@@ -1,4 +1,5 @@
 using Tyle.Api.Common;
+using Tyle.Core.Blocks;
 using Tyle.Core.Common;
 
 namespace Tyle.Api.Blocks;
@@ -8,7 +9,7 @@ public class BlockView : ImfType
     public ICollection<RdlClassifier> Classifiers { get; set; } = new List<RdlClassifier>();
     public RdlPurpose? Purpose { get; set; }
     public string? Notation { get; set; }
-    public string? Symbol { get; set; }
+    public Symbol? Symbol { get; set; }
     public Aspect? Aspect { get; set; }
     public ICollection<TerminalTypeReferenceView> Terminals { get; set; } = new List<TerminalTypeReferenceView>();
     public ICollection<AttributeTypeReferenceView> Attributes { get; set; } = new List<AttributeTypeReferenceView>();
