cd4c7317771ba37b97581cae06987cf9b63a1d00
Jørgen Drejer Norheim <122889083+jorgen-norheim@users.noreply.github.com>
2023-12-11 12:59:34 +0100

    Removed unused components (#904)

--- src/client/src/components/FormAttributes/prepareAttributes.ts
+++
@@ -1,7 +0,0 @@
-import { AttributeView } from "types/attributes/attributeView";
-
-export const prepareAttributes = (attributes?: AttributeView[]) => {
-  if (!attributes || attributes.length === 0) return [];
-
-  return attributes.sort((a, b) => a.name.localeCompare(b.name));
-};
