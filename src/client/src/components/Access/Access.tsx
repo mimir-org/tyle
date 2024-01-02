178532a2f9516b40a0739d86aa585ea1b765ca90
Andreas Kristensen <81438579+AndyEPandy@users.noreply.github.com>
2024-01-02 10:13:21 +0100

    Added access component (#930)
    * Added access component
    
    * run format
    
    ---------
    
    Co-authored-by: Andreas Eikefet Kristensen <andreas.kristensen@bouvet.no>

--- src/client/src/components/Access/Access.tsx
+++ src/client/src/components/Access/Access.tsx
@@ -1,24 +1,51 @@
-import { Flexbox, Text } from "@mimirorg/component-library";
+import { Flexbox } from "@mimirorg/component-library";
 import SettingsSection from "components/SettingsSection";
 import { useTheme } from "styled-components";
+import { GetAllRolesMapped, GetAllUsersMapped, roleFilters, toUserRoleRequest } from "../Roles/Roles.helpers";
+import { useState } from "react";
+import UserList from "../Roles/UserList";
+import UserListItem from "../Roles/UserListItem";
+import RoleDialog from "../Roles/RoleDialog";
+import { useGetCurrentUser } from "../../api/user.queries";
+import { mapUserViewToUserItem } from "../../helpers/mappers.helpers";
+import { useUpdateUserRole } from "../../api/authorize.queries";
+import { UserItem } from "../../types/userItem";
+import { useSubmissionToast } from "../../helpers/form.helpers";
 
 const Access = () => {
   const theme = useTheme();
-  //const pendingUsersQuery = useGetPendingUsers();
 
-  //const users = pendingUsersQuery.data?.sort((a, b) => a.firstName.localeCompare(b.firstName)) ?? [];
-  //const showPlaceholder = users && users.length === 0;
+  const [selectedRoleFilter] = useState(roleFilters[0]?.label);
+  const userQuery = useGetCurrentUser();
+  const currentUser = userQuery?.data != null ? mapUserViewToUserItem(userQuery.data) : undefined;
+  const users = GetAllUsersMapped().filter((e) => e.roles.length === 0);
+  const roles = GetAllRolesMapped();
+  const updateUserRoleMutation = useUpdateUserRole();
+  const toast = useSubmissionToast("permission");
+  const filteredUsers = (): UserItem[] => {
+    const excludingCurrentUserList = users.filter((user) => user.id !== currentUser?.id);
+    if (selectedRoleFilter === "All") return excludingCurrentUserList.filter((user) => user.id !== currentUser?.id);
+    if (selectedRoleFilter === "None") return excludingCurrentUserList.filter((user) => user.roles.length === 0);
+    return excludingCurrentUserList.filter((user) => user.roles.includes(selectedRoleFilter));
+  };
 
+  const handleRoleChange = (user: UserItem, newRole: string | undefined) => {
+    const newRoleId = roles.find((r) => r.roleName === newRole)?.roleId ?? "";
+    toast(updateUserRoleMutation.mutateAsync(toUserRoleRequest(user.id, newRoleId)));
+  };
   return (
-    <SettingsSection title="Access">
-      <Text variant={"title-medium"} spacing={{ mb: theme.mimirorg.spacing.l }}>
-        New users
-      </Text>
-      <Flexbox flexDirection={"column"} gap={theme.mimirorg.spacing.xxxl}>
-        {/*showPlaceholder && <AccessPlaceholder text="No new users have requested access" />*/}
-        {/*users.map((user) => (
-          <RoleCard key={user.id} user={mapMimirorgUserCmToUserItem(user)} />
-        ))*/}
+    <SettingsSection title="Roles">
+      <Flexbox flexDirection={"column"} gap={theme.mimirorg.spacing.xxl}>
+        <UserList title="Users">
+          {filteredUsers().map((user) => (
+            <UserListItem
+              key={user.id}
+              name={user.name}
+              role={user.roles}
+              action={<RoleDialog user={user} handleRoleChange={handleRoleChange} />}
+            />
+          ))}
+        </UserList>
       </Flexbox>
     </SettingsSection>
   );
